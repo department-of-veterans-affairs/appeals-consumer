d5472e79f1fed470587bd3f1ce436e1e6b78560b
Eli Brown <elilogbro@gmail.com>
2024-01-17 13:00:13 -0500

    created message payload validator module

---
+++ app/models/concerns/message_payload_validator.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# This module is used to validate incoming attribute names and data types
+# Any class wishing to use this module should have a method called #attribute_types
+# Reference decision_review_created#attribute_types for more information
+module MessagePayloadValidator
+  private
+
+  # Validates attribute names and data types
+  def validate(attributes)
+    validate_attribute_names(attributes.keys)
+
+    attribute_types.each do |name, type|
+      validate_attribute_data_types(attributes, name, type)
+    end
+  end
+
+  # Checks that incoming attribute names match the attributes listed in class#attribute_types
+  # Fails out of workflow if there is an unknown attribute found
+  def validate_attribute_names(attribute_names)
+    unknown_attributes = attribute_names.reject { |attr| attribute_types.key?(attr) }
+
+    fail ArgumentError, "Unknown attributes: #{unknown_attributes.join(', ')}" unless unknown_attributes.empty?
+  end
+
+  # Checks that incoming attribute types match the attribute types listed in class#attribute_types
+  # Fails out of workflow if there is an unexpected data type found
+  def validate_attribute_data_types(attributes, name, type)
+    value = attributes[name]
+    allowed_types = type.is_a?(Array) ? type : [type]
+
+    unless allowed_types.any? { |type| value.is_a?(type) }
+      fail ArgumentError, "#{name} must be one of the allowed types #{allowed_types}, got #{value.class}."
+    end
+  end
+end
