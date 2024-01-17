6225e9310c907534e38450dc4f931af0391e0acc
Eli Brown <elilogbro@gmail.com>
2024-01-17 12:54:52 -0500

    renamed parameter

--- app/models/decision_review_created.rb
+++ app/models/decision_review_created.rb
@@ -15,17 +15,17 @@ class DecisionReviewCreated
   # When DecisionReviewCreated.new(message_payload) is called, this method will
   # validate the attribute names and data types, assign the incoming attributes to defined keys, and
   # create DecisionReviewIssue instances for each object in the message_payload's decision_review_issues array
-  def initialize(attributes = {})
-    validate(attributes)
-    assign(attributes)
-    extract_decision_review_issues(attributes[:decision_review_issues])
+  def initialize(message_payload = {})
+    validate(message_payload)
+    assign(message_payload)
+    extract_decision_review_issues(message_payload[:decision_review_issues])
   end
 
   private
 
   # rubocop:disable Metrics/AbcSize
   # rubocop:disable Metrics/MethodLength
-  # Lists the attributes and corresponding data types
+  # Lists the message_payload and corresponding data types
   # Data types are listed in an array when the value isn't limited to one data type
   # For example, originated_from_vacols_issue could be a boolean OR nil
   def attribute_types
@@ -56,29 +56,29 @@ class DecisionReviewCreated
   end
 
   # Assigns attributes from the message_payload to defined keys
-  def assign(attributes)
-    @claim_id = attributes[:claim_id]
-    @decision_review_type = attributes[:decision_review_type]
-    @veteran_first_name = attributes[:veteran_first_name]
-    @veteran_last_name = attributes[:veteran_last_name]
-    @veteran_participant_id = attributes[:veteran_participant_id]
-    @veteran_file_number = attributes[:veteran_file_number]
-    @claimant_participant_id = attributes[:claimant_participant_id]
-    @ep_code = attributes[:ep_code]
-    @ep_code_category = attributes[:ep_code_category]
-    @claim_received_date = attributes[:claim_received_date]
-    @claim_lifecycle_status = attributes[:claim_lifecycle_status]
-    @payee_code = attributes[:payee_code]
-    @modifier = attributes[:modifier]
-    @originated_from_vacols_issue = attributes[:originated_from_vacols_issue]
-    @informal_conference_requested = attributes[:informal_conference_requested]
-    @same_station_requested = attributes[:same_station_requested]
-    @intake_creation_time = attributes[:intake_creation_time]
-    @claim_creation_time = attributes[:claim_creation_time]
-    @created_by_username = attributes[:created_by_username]
-    @created_by_station = attributes[:created_by_station]
-    @created_by_application = attributes[:created_by_application]
-    @decision_review_issues = attributes[:decision_review_issues]
+  def assign(message_payload)
+    @claim_id = message_payload[:claim_id]
+    @decision_review_type = message_payload[:decision_review_type]
+    @veteran_first_name = message_payload[:veteran_first_name]
+    @veteran_last_name = message_payload[:veteran_last_name]
+    @veteran_participant_id = message_payload[:veteran_participant_id]
+    @veteran_file_number = message_payload[:veteran_file_number]
+    @claimant_participant_id = message_payload[:claimant_participant_id]
+    @ep_code = message_payload[:ep_code]
+    @ep_code_category = message_payload[:ep_code_category]
+    @claim_received_date = message_payload[:claim_received_date]
+    @claim_lifecycle_status = message_payload[:claim_lifecycle_status]
+    @payee_code = message_payload[:payee_code]
+    @modifier = message_payload[:modifier]
+    @originated_from_vacols_issue = message_payload[:originated_from_vacols_issue]
+    @informal_conference_requested = message_payload[:informal_conference_requested]
+    @same_station_requested = message_payload[:same_station_requested]
+    @intake_creation_time = message_payload[:intake_creation_time]
+    @claim_creation_time = message_payload[:claim_creation_time]
+    @created_by_username = message_payload[:created_by_username]
+    @created_by_station = message_payload[:created_by_station]
+    @created_by_application = message_payload[:created_by_application]
+    @decision_review_issues = message_payload[:decision_review_issues]
   end
   # rubocop:enable Metrics/AbcSize
   # rubocop:enable Metrics/MethodLength
@@ -86,7 +86,7 @@ class DecisionReviewCreated
   # Creates instances of DecisionReviewIssue, validates attributes, and assigns attributes
   # for each object in the decision_review_issues array
   def extract_decision_review_issues(decision_review_issues)
-    decision_review_issues&.map { |issue_attributes| DecisionReviewIssue.new(issue_attributes) }
+    decision_review_issues&.map { |issue_attrs| DecisionReviewIssue.new(issue_attrs) }
   end
 end
 
@@ -100,17 +100,17 @@ class DecisionReviewIssue
               :eligible, :eligibility_result, :time_override, :time_override_reason, :contested, :soc_opt_in,
               :legacy_appeal_id, :legacy_appeal_issue_id
 
-  # When DecisionReviewIssue.new(issue_attributes) is called, this method will
+  # When DecisionReviewIssue.new(issue_attrs) is called, this method will
   # validate the attribute names and data types and assign the incoming attributes to defined keys
-  def initialize(attributes = {})
-    validate(attributes)
-    assign(attributes)
+  def initialize(message_payload = {})
+    validate(message_payload)
+    assign(message_payload)
   end
 
   private
 
   # Lists the attributes and corresponding data types
-  # Data types are listed in an array when the value isn't limited to one data type
+  # Data types are stored in an array when the value isn't limited to one data type
   # For example, time_override could be a boolean OR nil
   def attribute_types
     {
@@ -135,25 +135,25 @@ class DecisionReviewIssue
     }.freeze
   end
 
-  # Assigns attributes from issue_attributes to defined keys
-  def assign(attributes)
-    @contention_id = attributes[:contention_id]
-    @associated_caseflow_request_issue_id = attributes[:associated_caseflow_request_issue_id]
-    @unidentified = attributes[:unidentified]
-    @prior_rating_decision_id = attributes[:prior_rating_decision_id]
-    @prior_non_rating_decision_id = attributes[:prior_non_rating_decision_id]
-    @prior_decision_text = attributes[:prior_decision_text]
-    @prior_decision_type = attributes[:prior_decision_type]
-    @prior_decision_notification_date = attributes[:prior_decision_notification_date]
-    @prior_decision_diagnostic_code = attributes[:prior_decision_diagnostic_code]
-    @prior_decision_rating_percentage = attributes[:prior_decision_rating_percentage]
-    @eligible = attributes[:eligible]
-    @eligibility_result = attributes[:eligibility_result]
-    @time_override = attributes[:time_override]
-    @time_override_reason = attributes[:time_override_reason]
-    @contested = attributes[:contested]
-    @soc_opt_in = attributes[:soc_opt_in]
-    @legacy_appeal_id = attributes[:legacy_appeal_id]
-    @legacy_appeal_issue_id = attributes[:legacy_appeal_issue_id]
+  # Assigns attributes from issue_attrs to defined keys
+  def assign(message_payload)
+    @contention_id = message_payload[:contention_id]
+    @associated_caseflow_request_issue_id = message_payload[:associated_caseflow_request_issue_id]
+    @unidentified = message_payload[:unidentified]
+    @prior_rating_decision_id = message_payload[:prior_rating_decision_id]
+    @prior_non_rating_decision_id = message_payload[:prior_non_rating_decision_id]
+    @prior_decision_text = message_payload[:prior_decision_text]
+    @prior_decision_type = message_payload[:prior_decision_type]
+    @prior_decision_notification_date = message_payload[:prior_decision_notification_date]
+    @prior_decision_diagnostic_code = message_payload[:prior_decision_diagnostic_code]
+    @prior_decision_rating_percentage = message_payload[:prior_decision_rating_percentage]
+    @eligible = message_payload[:eligible]
+    @eligibility_result = message_payload[:eligibility_result]
+    @time_override = message_payload[:time_override]
+    @time_override_reason = message_payload[:time_override_reason]
+    @contested = message_payload[:contested]
+    @soc_opt_in = message_payload[:soc_opt_in]
+    @legacy_appeal_id = message_payload[:legacy_appeal_id]
+    @legacy_appeal_issue_id = message_payload[:legacy_appeal_issue_id]
   end
 end
