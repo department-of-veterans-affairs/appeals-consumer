c3def18c81be900bce0144ea024ba24a3b122f30
nkirby-va <nicholas.kirby@va.gov>
2024-01-22 10:16:41 -0500

    re-arrange decision_review_created_event

--- app/models/topics/decision_review_created_topic/decision_review_created_event.rb
+++ app/models/topics/decision_review_created_topic/decision_review_created_event.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 # A subclass of Event, representing the DecisionReviewCreated Kafka topic event.
-class Topics::DecisionReviewCreatedTopic::Events::DecisionReviewCreatedEvent < Event
+class Topics::DecisionReviewCreatedTopic::DecisionReviewCreatedEvent < Event
 end
