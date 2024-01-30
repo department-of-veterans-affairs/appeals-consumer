4d90a899a173f7d658d80efbfaa81e4e2222c216
Eli Brown <elilogbro@gmail.com>
2024-01-30 09:14:51 -0500

    spec for builder classes

--- spec/models/builders/claim_review_builder_spec.rb
+++ spec/models/builders/claim_review_builder_spec.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
-describe ClaimReviewBuilder do
-
+describe Builders::ClaimReviewBuilder do
+  describe "#new" do
+    subject { described_class.new }
+    it "creates an instance of the class" do
+      expect(subject).to be_an_instance_of(described_class)
+    end
+  end
 end
--- spec/models/builders/end_product_establishment_builder_spec.rb
+++ spec/models/builders/end_product_establishment_builder_spec.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
-describe EndProductEstablishmentBuilder do
-
+describe Builders::EndProductEstablishmentBuilder do
+  describe "#new" do
+    subject { described_class.new }
+    it "creates an instance of the class" do
+      expect(subject).to be_an_instance_of(described_class)
+    end
+  end
 end
