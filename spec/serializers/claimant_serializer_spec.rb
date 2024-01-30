6f1bec0bcad00e9028f8037ce3b1104c9ba9fbc2
Eli Brown <elilogbro@gmail.com>
2024-01-30 12:25:26 -0500

    serializer specs

--- spec/serializers/claimant_serializer_spec.rb
+++ spec/serializers/claimant_serializer_spec.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 describe ClaimantSerializer do
+  let(:claimant) { build(:claimant) }
+  subject { described_class.new(claimant).attributes }
 
+  it "serializes the attributes" do
+    expect(subject).to eq(
+      "payee_code" => claimant.payee_code,
+      "type" => claimant.type,
+      "participant_id" => claimant.participant_id,
+      "name_suffix" => claimant.name_suffix
+    )
+  end
 end
--- spec/serializers/veteran_serializer_spec.rb
+++ spec/serializers/veteran_serializer_spec.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
 describe VeteranSerializer do
+  let(:veteran) { build(:veteran) }
+  subject { described_class.new(veteran).attributes }
 
+  it "serializes the attributes" do
+    expect(subject).to eq(
+      "participant_id" => veteran.participant_id,
+      "bgs_last_synced_at" => veteran.bgs_last_synced_at,
+      "closest_regional_office" => veteran.closest_regional_office,
+      "date_of_death" => veteran.date_of_death,
+      "date_of_death_reported_at" => veteran.date_of_death_reported_at,
+      "name_suffix" => veteran.name_suffix
+    )
+  end
 end
