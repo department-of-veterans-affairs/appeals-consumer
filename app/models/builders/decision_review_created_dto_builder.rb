a7d7e45a149ac4b863b08503b7d8d44ef5db39b5
nkirby-va <nicholas.kirby@va.gov>
2024-01-30 12:03:23 -0500

    clean up

--- app/models/builders/decision_review_created_dto_builder.rb
+++ app/models/builders/decision_review_created_dto_builder.rb
@@ -2,12 +2,14 @@
 
 # This class is ultimately responsible for constructing a DecisionReviewCreated payload
 # that will be sent to Caseflow.
+# :reek:TooManyInstanceVariables
+# :reek:TooManyMethods
 class Builders::DecisionReviewCreatedDtoBuilder
+  attr_reader :vet_file_number, :vet_ssn, :vet_first_name, :vet_middle_name, :vet_last_name, :claimant_ssn,
+              :claimant_dob, :claimant_first_name, :claimant_middle_name, :claimant_last_name, :claimant_email
 
-  attr_reader :vet_file_number, :vet_ssn, :vet_first_name, :vet_middle_name, :vet_last_name,
-    :claimant_ssn, :claimant_dob, :claimant_first_name, :claimant_middle_name, :claimant_last_name
-    :claimant_email
-
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/AbcSize
   def initialize(dcr_event)
     @decision_review_created = DecisionReviewCreated.new(dcr_event.message_payload)
     @css_id = @decision_review_created.created_by_username
@@ -20,18 +22,20 @@ class Builders::DecisionReviewCreatedDtoBuilder
     @end_product_establishment = build_end_product_establishment
     @request_issues = build_request_issues
     @vet_file_number = @decision_review_created.veteran_file_number
-    @vet_ssn = vet_ssn
+    @vet_ssn = retrieve_vet_ssn
     @vet_first_name = @decision_review_created.veteran_first_name
-    @vet_middle_name = vet_middle_name
+    @vet_middle_name = retrieve_vet_middle_name
     @vet_last_name = @decision_review_created.veteran_last_name
-    @claimant_ssn = claimant_ssn
-    @claimant_dob = claimant_dob
-    @claimant_first_name = claimant_first_name
-    @claimant_middle_name = claimant_middle_name
-    @claimant_last_name = claimant_last_name
-    @claimant_email = claimant_email
+    @claimant_ssn = retrieve_claimant_ssn
+    @claimant_dob = retrieve_claimant_dob
+    @claimant_first_name = retrieve_claimant_first_name
+    @claimant_middle_name = retrieve_claimant_middle_name
+    @claimant_last_name = retrieve_claimant_last_name
+    @claimant_email = retrieve_claimant_email
     @hash_response = build_hash_response
   end
+  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/AbcSize
 
   private
 
@@ -54,35 +58,26 @@ class Builders::DecisionReviewCreatedDtoBuilder
   def build_end_product_establishment
     Builders::EndProductEstablishmentBuilder.build(@decision_review_created)
   end
-  
+
   def build_request_issues
     Builders::RequestIssueBuilder.build(@decision_review_created.decision_review_issues)
   end
 
-  def vet_ssn
-  end
+  def retrieve_vet_ssn; end
 
-  def vet_middle_name
-  end
+  def retrieve_vet_middle_name; end
 
-  def claimant_ssn
-  end
+  def retrieve_claimant_ssn; end
 
-  def claimant_dob
-  end
+  def retrieve_claimant_dob; end
 
-  def claimant_first_name
-  end
+  def retrieve_claimant_first_name; end
 
-  def claimant_middle_name
-  end
+  def retrieve_claimant_middle_name; end
 
-  def claimant_last_name
-  end
+  def retrieve_claimant_last_name; end
 
-  def claimant_email
-  end
+  def retrieve_claimant_email; end
 
-  def build_hash_response
-  end
+  def retrieve_build_hash_response; end
 end
