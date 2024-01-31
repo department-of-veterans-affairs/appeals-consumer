9c5f118e00a42bb4e4e2f50898931b04decba239
nkirby-va <nicholas.kirby@va.gov>
2024-01-30 20:44:09 -0500

    add tests for dcr dto builder

---
+++ spec/models/builders/decision_review_created_dto_builder_spec.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Builders::DecisionReviewCreatedDtoBuilder, type: :model do
+  describe "#initialize" do
+    it "should return a new empty object dcr dto builder" do
+    end
+  end
+  
+  describe "#_build_decision_review_created" do
+    it "should return a new DecisionReviewCreated object" do
+    end
+  end
+
+  describe "#_build_intake" do
+    it "should return built intake object" do
+    end
+  end
+
+  describe "#_build_veteran" do
+    it "should return built veteran object" do
+    end
+  end
+
+  describe "#_build_claimant" do
+    it "should return built claimant object" do
+    end
+  end
+
+  describe "#_build_claim_review" do
+    it "should return built claim review object" do
+    end
+  end
+
+  describe "#_build_end_product_establishment" do
+    it "should return built epe object" do
+    end
+  end
+
+  describe "#_build_request_issues" do
+    it "should return built request issues object" do
+    end
+  end
+
+  describe "#_retrieve_vet_ssn" do
+    it "should return vet ssn" do
+    end
+  end
+
+  describe "#_retrieve_vet_middle_name" do
+    it "should return vet middle name" do
+    end
+  end
+
+  describe "#_retrieve_claimant_ssn" do
+    it "should return claimant ssn" do
+    end
+  end
+
+  describe "#_retrieve_claimant_dob" do
+    it "should return claimant dob" do
+    end
+  end
+
+  describe "#_retrieve_claimant_first_name" do
+    it "should return cliamant first name" do
+    end
+  end
+
+  describe "#_retrieve_claimant_middle_name" do
+    it "should return claimant middle name" do
+    end
+  end
+
+  describe "#_retrieve_claimant_last_name" do
+    it "should return claimant last name" do
+    end
+  end
+
+  describe "#_retrieve_claimant_email" do
+    it "should return claimant email" do
+    end
+  end
+
+  describe "#_retrieve_build_hash_response" do
+    it "should return hash reponse object" do
+    end
+  end
+end
