2249f4205d12e4c371f7b5897c75a632d5b54b1d
Chris-Martine <christopher.martine@va.gov>
2024-01-31 08:00:04 -0500

    Move REDIS_URL_CACHE env var into env.sh, update application.rb

--- docker-bin/env.sh
+++ docker-bin/env.sh
@@ -6,3 +6,4 @@ export POSTGRES_PASSWORD=postgres
 export RAILS_ENV=development
 export DOCKERIZED=true
 export SHORYUKEN_SQS_ENDPOINT=http://localstack-consumer:4566
+export REDIS_URL_CACHE=redis://appeals-consumer-redis-1:6379/0
