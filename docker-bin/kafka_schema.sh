a23f4324865e07e9d07e48bda82047231bea86ba
david-whitlatch <david.whitlatch@va.gov>
2024-02-02 14:03:10 -0500

    change docker build to include uploading avro to schema registry during build

--- docker-bin/kafka_schema.sh
+++ docker-bin/kafka_schema.sh
@@ -1,4 +1,14 @@
 export TOPIC=DecisionReviewCreated
-export SCHEMA=$(jq tostring /usr/bin/$TOPIC.avsc)
 
-curl -s -X POST -H "Content-Type:application/json" -d "{\"schema\":$SCHEMA}" schema-registry:9021/subjects/$TOPIC/versions > /dev/null
+jq -Rs '{ schema: .}' /usr/bin/$TOPIC.avsc > temp-schema.json
+
+if [ ! -f temp-schema.json ]; then
+  echo "Failed to create temp schema file"
+  exit 1
+fi
+
+curl -s -X POST -H "Content-Type:application/vnd.schemaregistry.v1+json" --data @temp-schema.json http://schema-registry:9021/subjects/$TOPIC/versions > /dev/null
+
+echo "Avro was uploaded to the Schema-registry successfully"
+
+rm temp-schema.json
