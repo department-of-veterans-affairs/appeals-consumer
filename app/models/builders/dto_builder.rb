53a91695ff9aaf21b42268bd67634ea345521254
nkirby-va <nicholas.kirby@va.gov>
2024-02-02 09:24:31 -0500

    move anti pii methods to parent builder

--- app/models/builders/dto_builder.rb
+++ app/models/builders/dto_builder.rb
@@ -11,4 +11,20 @@ class Builders::DtoBuilder
     date_of_birth
     email
   ]
+
+  # :reek:UtilityFunction
+  def clean_pii(hashable_model)
+    hashable_model.as_json(except: Builders::DtoBuilder::PII_FIELDS)
+  end
+
+  # :reek:FeatureEnvy
+  def validate_no_pii(hash_response)
+    hash_response.extend Hashie::Extensions::DeepFind
+    Builders::DtoBuilder::PII_FIELDS.each do |field|
+      unless hash_response.deep_find_all(field).blank?
+        fail PIIFoundViolationError, "PII field detected: #{field}"
+      end
+    end
+    hash_response
+  end
 end
