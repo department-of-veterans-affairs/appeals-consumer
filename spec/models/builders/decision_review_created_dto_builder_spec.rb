6c834074fad08c576bff61a5280141e73edb8f12
nkirby-va <nicholas.kirby@va.gov>
2024-02-02 09:21:07 -0500

    update rspec skeleton

--- spec/models/builders/decision_review_created_dto_builder_spec.rb
+++ spec/models/builders/decision_review_created_dto_builder_spec.rb
@@ -4,97 +4,105 @@ require "rails_helper"
 
 RSpec.describe Builders::DecisionReviewCreatedDtoBuilder, type: :model do
   describe "#initialize" do
-    it "should return a new empty object dcr dto builder" do
+    context "when a decision_review_created object is found" do
+      it "should return a DecisionReviewCreatedDtoBuilder object with response and pii attributes(not in payload)" do
+      end
+    end
+    context "when a decision_review_created object is NOT found" do
+      it "should return a new empty object dcr dto builder" do
+      end
     end
   end
 
-  describe "#_build_decision_review_created" do
-    it "should return a new DecisionReviewCreated object" do
+  context "when we instantiate an empty DecisionReviewCreatedDtoBuilder object" do
+    describe "#_build_decision_review_created" do
+      it "should return a new DecisionReviewCreated object" do
+      end
     end
-  end
 
-  describe "#_assign" do
-    it "should assign attributes of dcr dto builder correctly on correct payload" do
+    describe "#_assign" do
+      it "should assign attributes of dcr dto builder correctly on correct payload" do
+      end
     end
-  end
 
-  describe "#_reset" do
-    it "should assign attributes of dcr dto builder correctly on correct payload" do
+    describe "#_reset" do
+      it "should assign attributes of dcr dto builder correctly on correct payload" do
+      end
     end
-  end
 
-  describe "#_build_intake" do
-    it "should return built intake object" do
+    describe "#_build_intake" do
+      it "should return built intake object" do
+      end
     end
-  end
 
-  describe "#_build_veteran" do
-    it "should return built veteran object" do
+    describe "#_build_veteran" do
+      it "should return built veteran object" do
+      end
     end
-  end
 
-  describe "#_build_claimant" do
-    it "should return built claimant object" do
+    describe "#_build_claimant" do
+      it "should return built claimant object" do
+      end
     end
-  end
 
-  describe "#_build_claim_review" do
-    it "should return built claim review object" do
+    describe "#_build_claim_review" do
+      it "should return built claim review object" do
+      end
     end
-  end
 
-  describe "#_build_end_product_establishment" do
-    it "should return built epe object" do
+    describe "#_build_end_product_establishment" do
+      it "should return built epe object" do
+      end
     end
-  end
 
-  describe "#_build_request_issues" do
-    it "should return built request issues object" do
+    describe "#_build_request_issues" do
+      it "should return built request issues object" do
+      end
     end
-  end
 
-  describe "#_retrieve_vet_ssn" do
-    it "should return vet ssn" do
+    describe "#_retrieve_vet_ssn" do
+      it "should return vet ssn" do
+      end
     end
-  end
 
-  describe "#_retrieve_vet_middle_name" do
-    it "should return vet middle name" do
+    describe "#_retrieve_vet_middle_name" do
+      it "should return vet middle name" do
+      end
     end
-  end
 
-  describe "#_retrieve_claimant_ssn" do
-    it "should return claimant ssn" do
+    describe "#_retrieve_claimant_ssn" do
+      it "should return claimant ssn" do
+      end
     end
-  end
 
-  describe "#_retrieve_claimant_dob" do
-    it "should return claimant dob" do
+    describe "#_retrieve_claimant_dob" do
+      it "should return claimant dob" do
+      end
     end
-  end
 
-  describe "#_retrieve_claimant_first_name" do
-    it "should return cliamant first name" do
+    describe "#_retrieve_claimant_first_name" do
+      it "should return cliamant first name" do
+      end
     end
-  end
 
-  describe "#_retrieve_claimant_middle_name" do
-    it "should return claimant middle name" do
+    describe "#_retrieve_claimant_middle_name" do
+      it "should return claimant middle name" do
+      end
     end
-  end
 
-  describe "#_retrieve_claimant_last_name" do
-    it "should return claimant last name" do
+    describe "#_retrieve_claimant_last_name" do
+      it "should return claimant last name" do
+      end
     end
-  end
 
-  describe "#_retrieve_claimant_email" do
-    it "should return claimant email" do
+    describe "#_retrieve_claimant_email" do
+      it "should return claimant email" do
+      end
     end
-  end
 
-  describe "#_build_hash_response" do
-    it "should return hash reponse object" do
+    describe "#_build_hash_response" do
+      it "should return hash reponse object" do
+      end
     end
   end
 end
---
+++ spec/models/builders/dto_builder_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Builders::DtoBuilder, type: :model do
+  describe "#clean_pii" do
+    context "when there are multiple PII attributes on an instance of a payload model" do
+      it "should return a hash of a model without any PII fields" do
+      end
+    end
+    context "when a PII field is an attribute on an instance of a payload model" do
+      it "should return a hash of a model without any PII fields" do
+      end
+    end
+    context "when there is no PII attribute on an instance of a payload model" do
+      it "should return a hash of a model without any PII fields" do
+      end
+    end
+  end
+
+  describe "#validate_no_pii" do
+    context "when no PII field is found in the hash_response" do
+      it "should return an unchanged hash_response" do
+      end
+    end
+    context "when there IS A PII field found in the hash_response" do
+      it "should throw a PIIFoundViolationError" do
+      end
+    end
+  end
+end
