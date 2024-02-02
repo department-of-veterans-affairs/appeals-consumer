1f49fa42b982b4918f35ba6451b3fda59a6270e3
nkirby-va <nicholas.kirby@va.gov>
2024-02-02 14:54:38 -0500

    mock with hash until instance available on other pr

--- spec/models/builders/dto_builder_spec.rb
+++ spec/models/builders/dto_builder_spec.rb
@@ -1,17 +1,53 @@
 # frozen_string_literal: true
 
 RSpec.describe Builders::DtoBuilder, type: :model do
+  subject { Builders::DtoBuilder.new }
+
   describe "#clean_pii" do
     context "when there are multiple PII attributes on an instance of a payload model" do
+      let(:sample_hash) do
+        {
+          "id": 1,
+          "ssn": "111111",
+          "date_of_birth": Time.current
+        }.as_json
+      end
+
       it "should return a hash of a model without any PII fields" do
+        clean_hash = { "id": 1 }.as_json
+        expect(subject.clean_pii(sample_hash)).to eq clean_hash
       end
     end
     context "when a PII field is an attribute on an instance of a payload model" do
+      let(:sample_hash) do
+        {
+          "id": 1,
+          "ssn": "111111"
+        }.as_json
+      end
       it "should return a hash of a model without any PII fields" do
+        clean_hash = { "id": 1 }.as_json
+        expect(subject.clean_pii(sample_hash)).to eq clean_hash
+      end
+    end
+    context "when there are deep PII fields in instance of payload model" do
+      let(:sample_hash) do
+        {
+          "id": 1,
+          "person": {
+            "p_id": 1,
+            "ssn": "111111"
+          }
+        }.as_json
+      end
+      it "shoud return a hash of a model without any PII fields" do
+        clean_hash = {"id": 1, "person": {"p_id": 1}}.as_json
+        expect(subject.clean_pii(sample_hash)).to eq clean_hash
       end
     end
     context "when there is no PII attribute on an instance of a payload model" do
       it "should return a hash of a model without any PII fields" do
+        #byebug
       end
     end
   end
