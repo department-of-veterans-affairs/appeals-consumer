ee5b705891d882551aa6ff63b285c8f64ad6423d
Kodi Shiflett <kodishiflett@gmail.com>
2024-02-06 10:11:30 -0500

    APPEALS-36737: Add BISService to Consumer App (#18)
    * adding gem, fakes, services and spec files
    
    * update gemlock
    
    * gemfile lock update
    
    * add store_veteran_info
    
    * vbms_id to file_number
    
    * change logger vbms id to file number

---
+++ app/mappers/power_of_attorney_mapper.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module PowerOfAttorneyMapper
+
+  # This is here so when we include this module
+  # in classes (e.g. in PoaRepository),
+  # the class itself and not just its instance
+  # get the methods from this class.
+  def self.included(base)
+    base.extend(PowerOfAttorneyMapper)
+  end
+
+  def get_limited_poas_hash_from_bis(bis_response)
+    return unless bis_response
+
+    limited_poas_hash = {}
+
+    Array.wrap(bis_response).map do |lpoa|
+      limited_poas_hash[lpoa[:bnft_claim_id]] = {
+        limited_poa_code: lpoa[:poa_cd],
+        limited_poa_access: lpoa[:authzn_poa_access_ind]
+      }
+    end
+
+    limited_poas_hash
+  end
+end 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,7 @@ require 'support/factory_bot'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -33,6 +33,9 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+   # Allows us to use shorthand FactoryBot methods.
+   config.include FactoryBot::Syntax::Methods
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
