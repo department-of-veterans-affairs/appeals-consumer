1774e1dcc303465ddeb95cfa09039b4b7f38009e
Matt Ray <matthew.ray@va.gov>
2024-02-12 15:25:05 -0600

    Adding api application controller with auth

---
+++ app/controllers/api/v1/application_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Api::V1::ApplicationController < ApplicationController
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate
+
+  private
+
+  def authenticate_with_token
+    @authenticate_with_token ||= authenticate_with_http_token do |token, _options|
+      token == Rails.application.config.api_key
+    end
+  end
+
+  def unauthorized
+    render json: { status: "unauthorized" }, status: 401
+  end
+
+  def authenticate
+    if !authenticate_with_token
+      unauthorized
+    end
+  end
+end
