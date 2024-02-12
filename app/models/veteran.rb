70f6dfb3194c9f82291d93ec7d34e6a1db5d6fd5
Eli Brown <elilogbro@gmail.com>
2024-02-12 08:46:38 -0500

    removed unused attrs

--- app/models/veteran.rb
+++ app/models/veteran.rb
@@ -2,7 +2,6 @@
 
 # This class should be instantiated via Builders::VeteranBuilder
 class Veteran
-  attr_accessor :participant_id, :bgs_last_synced_at, :closest_regional_office, :date_of_death,
-                :date_of_death_reported_at, :name_suffix, :ssn, :file_number, :first_name,
+  attr_accessor :participant_id, :bgs_last_synced_at, :date_of_death, :name_suffix, :ssn, :file_number, :first_name,
                 :middle_name, :last_name
 end
