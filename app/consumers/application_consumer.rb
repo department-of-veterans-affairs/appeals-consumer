7e9058b395e5f47093820d4608d7fa50e01dbc91
david-whitlatch <david.whitlatch@va.gov>
2024-02-13 13:17:26 -0500

    change drc consumer to abstract functionality to parent

--- app/consumers/application_consumer.rb
+++ app/consumers/application_consumer.rb
@@ -4,4 +4,73 @@
 # You can rename it if it would conflict with your current code base (in case you're integrating
 # Karafka with other frameworks)
 class ApplicationConsumer < Karafka::BaseConsumer
+  ERROR_MSG = "Error running"
+
+  private
+
+  def process_event(event)
+    # Check if the event is newly initialized and does not exists in the database already
+    if event&.new_record?
+      event.save
+      yield event if block_given?
+      log_consumption_job
+    else
+      log_repeat_consumption
+    end
+  rescue ActiveRecord::RecordInvalid => error
+    handle_error(error, extra_details(message))
+    nil # Return nil to indicate failure
+  end
+
+  def log_consumption_start(extra_details)
+    log_info("Starting consumption", extra_details)
+  end
+
+  def log_consumption_job
+    log_info("Dropped Event into processing job")
+  end
+
+  def log_repeat_consumption
+    log_info("Event record already exists. Skipping enqueueing job")
+  end
+
+  def log_consumption_end
+    log_info("Completed consumption of message")
+  end
+
+  def log_info(message, extra = {})
+    full_message = "[#{self.class.name}] #{message}"
+    full_message += " | #{extra.to_json}" unless extra.empty?
+    Karafka.logger.info(full_message)
+  end
+
+  # Handles errors and notifies via Slack and Sentry
+  def handle_error(error, extra_details = {})
+    notify_sentry(error, extra_details)
+    notify_slack
+  end
+
+  def notify_slack
+    slack_message = "#{ERROR_MSG} #{self.class.name}"
+    slack_message += " See Sentry event #{Sentry.last_event_id}" if Sentry.last_event_id.present?
+    slack_service.send_notification(slack_message, self.class.name)
+  end
+
+  def notify_sentry(error, extra_details)
+    Sentry.capture_exception(error) do |scope|
+      scope.set_extras({
+                         **extra_details,
+                         source: self.class.name
+                       })
+    end
+  end
+
+  # :reek:UtilityFunction
+  def slack_url
+    ENV["SLACK_DISPATCH_ALERT_URL"]
+  end
+
+  def slack_service
+    @slack_service ||= SlackService.new(url: slack_url)
+  end
 end
--- app/consumers/decision_review_created_consumer.rb
+++ app/consumers/decision_review_created_consumer.rb
@@ -1,30 +1,22 @@
 # frozen_string_literal: true
 
 class DecisionReviewCreatedConsumer < ApplicationConsumer
-  # Constants for hardcoded strings
-  ERROR_MSG = "Error running DecisionReviewCreatedConsumer"
-  CONSUMER_NAME = "DecisionReviewCreatedConsumer"
+  # Constant for hardcoded strings
   EVENT_TYPE = "Topics::DecisionReviewCreatedTopic::DecisionReviewCreatedEvent"
 
   def consume
     messages.map do |message|
-      log_consumption_start(message)
+      log_consumption_start(extra_details(message))
 
       event = handle_event_creation(message)
 
-      #  Perform the job with the created event
-      if event&.new_record?
-        event.save
-        DecisionReviewCreatedJob.perform_later(event)
-        log_consumption_job
-      else
-        log_repeat_consumption
+      process_event(event) do |new_event|
+        #  Perform the job with the created event
+        DecisionReviewCreatedJob.perform_later(new_event)
       end
-    rescue ActiveRecord::RecordInvalid => error
-      handle_error(error, message)
-      nil # Return nil to indicate failure
+
+      log_consumption_end
     end
-    log_consumption_end
   end
 
   private
@@ -39,63 +31,11 @@ class DecisionReviewCreatedConsumer < ApplicationConsumer
     end
   end
 
-  def log_consumption_start(message)
-    log_info("Starting consumption", extra_details(message))
-  end
-
-  def log_consumption_job
-    log_info("Dropped Event into processing job")
-  end
-
-  def log_repeat_consumption
-    log_info("Event record already exists. Skipping enqueueing job")
-  end
-
-  def log_consumption_end
-    log_info("Completed consumption of message")
-  end
-
-  def log_info(message, extra = {})
-    full_message = "[#{CONSUMER_NAME}] #{message}"
-    full_message += " | #{extra.to_json}" unless extra.empty?
-    Karafka.logger.info(full_message)
-  end
-
   def extra_details(message)
     {
       partition: message.metadata.partition,
       offset: message.metadata.offset,
       claim_id: message.payload.message["claim_id"]
     }
-  end
-
-  # Handles errors and notifies via Slack and Sentry
-  def handle_error(error, message)
-    notify_sentry(error, message)
-    notify_slack
-  end
-
-  def notify_slack
-    slack_message = ERROR_MSG
-    slack_message += " See Sentry event #{Sentry.last_event_id}" if Sentry.last_event_id.present?
-    slack_service.send_notification(slack_message, CONSUMER_NAME)
-  end
-
-  def notify_sentry(error, message)
-    Sentry.capture_exception(error) do |scope|
-      scope.set_extras({
-                         **extra_details(message),
-                         source: CONSUMER_NAME
-                       })
-    end
-  end
-
-  # :reek:UtilityFunction
-  def slack_url
-    ENV["SLACK_DISPATCH_ALERT_URL"]
-  end
-
-  def slack_service
-    @slack_service ||= SlackService.new(url: slack_url)
   end
 end
