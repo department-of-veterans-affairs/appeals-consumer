7ec1628ee84df39186b6904d79dd2f46dc54fd89
david-whitlatch <david.whitlatch@va.gov>
2024-02-13 16:52:52 -0500

    add commenting to the consumers

--- app/consumers/application_consumer.rb
+++ app/consumers/application_consumer.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
-# Application consumer from which all Karafka consumers should inherit
-# You can rename it if it would conflict with your current code base (in case you're integrating
-# Karafka with other frameworks)
+# This class serves as the base consumer for all Karafka message consumption within the application.
+# It abstracts common functionality needed across different consumers, such as event processing,
+# logging, and error handling. This setup promotes reuse and consistency in handling incoming
+# messages from Kafka topics.
 class ApplicationConsumer < Karafka::BaseConsumer
+  # Constant defined for generic error message prefix used in notifications.
   ERROR_MSG = "Error running"
 
   private
 
+  # processes a given event, saving it to the database if it's new and logging the operation.
   def process_event(event, extra_details)
-    # Check if the event is newly initialized and does not exists in the database already
+    # Determines if the event is new and not yet saved in the database.
     if event&.new_record?
       event.save
 
@@ -26,22 +29,27 @@ class ApplicationConsumer < Karafka::BaseConsumer
     end
   end
 
+  # Logs the start of an event consumption with extra details for diagnostic purposes.
   def log_consumption_start(extra_details)
     log_info("Starting consumption", extra_details)
   end
 
+  # Logs the successful enqueueing of an event consumption into a processing job.
   def log_consumption_job(extra_details)
     log_info("Dropped Event into processing job", extra_details)
   end
 
+  # Logs an attempt to process an event that already exists in the database, indicating no further action was taken.
   def log_repeat_consumption(extra_details)
     log_info("Event record already exists. Skipping enqueueing job", extra_details)
   end
 
+  # Logs the start of an event consumption with extra details for diagnostic purposes.
   def log_consumption_end(extra_details)
     log_info("Completed consumption of message", extra_details)
   end
 
+  # Utility method for logging information with a consisten format, including the class name and optional details.
   def log_info(message, extra = {})
     full_message = "[#{self.class.name}] #{message}"
     full_message += " | #{extra.to_json}" unless extra.empty?
@@ -54,12 +62,14 @@ class ApplicationConsumer < Karafka::BaseConsumer
     notify_slack
   end
 
+  # Sends an error notification message to a configured Slack channel, including a Sentry event id if available.
   def notify_slack
     slack_message = "#{ERROR_MSG} #{self.class.name}"
     slack_message += " See Sentry event #{Sentry.last_event_id}" if Sentry.last_event_id.present?
     slack_service.send_notification(slack_message, self.class.name)
   end
 
+  # Reports an exception to Sentry, including additional details for debugging.
   def notify_sentry(error, extra_details)
     Sentry.capture_exception(error) do |scope|
       scope.set_extras({
--- app/consumers/decision_review_created_consumer.rb
+++ app/consumers/decision_review_created_consumer.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
+# This class is a specialized consumer that processes message related to the Decision Review Created kafka topic.
+# It processes messages by creating or finding events in the database and enqueues jobs for further processing.
 class DecisionReviewCreatedConsumer < ApplicationConsumer
-  # Constant for hardcoded strings
+  # Defines the event type string for decision review created events to standardize the event handling process.
   EVENT_TYPE = "Topics::DecisionReviewCreatedTopic::DecisionReviewCreatedEvent"
 
+  # Consumes messages from the Kafka topic, processing each message to handle event creation,
+  # job enqueueing and error management. It iterates over each message, logging the start and end of
+  # the consumption process, and uses a block within `process_event` for job execution.
   def consume
     messages.map do |message|
       extra_details = extra_details(message)
@@ -13,11 +18,13 @@ class DecisionReviewCreatedConsumer < ApplicationConsumer
       begin
         event = handle_event_creation(message)
 
+        # Processes the event with additional logic provided in the block for job enqueueing.
         process_event(event, extra_details) do |new_event|
-          #  Perform the job with the created event
+          # Enqueues a job for further processing of the newly created event.
           DecisionReviewCreatedJob.perform_later(new_event)
         end
       rescue ActiveRecord::RecordInvalid => error
+        # Handles any ActiveRecord validation errors by logging and notifying the error.
         handle_error(error, extra_details)
         nil # Return nil to indicate failure
       end
@@ -28,6 +35,9 @@ class DecisionReviewCreatedConsumer < ApplicationConsumer
 
   private
 
+  # Attempts to find or initialize a new decision review created event based on message metadata.
+  # This method ensures that each event is uniquely identified by its poartition and offset,
+  # preventing duplicate processing of the same event.
   def handle_event_creation(message)
     Topics::DecisionReviewCreatedTopic::DecisionReviewCreatedEvent.find_or_initialize_by(
       partition: message.metadata.partition,
@@ -38,6 +48,8 @@ class DecisionReviewCreatedConsumer < ApplicationConsumer
     end
   end
 
+  # Extracts and returns a hash of extra details from the message for logging and diagnostic purposes.
+  # These details include the kafka partition, offset, and the claim ID extracted from the message payload.
   def extra_details(message)
     {
       partition: message.metadata.partition,
