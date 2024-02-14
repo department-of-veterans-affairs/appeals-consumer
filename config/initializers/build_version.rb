c46d61187140e3925198075d9257edd3b9115cb8
Chris-Martine <christopher.martine@va.gov>
2024-02-14 10:54:39 -0500

    Add health check rspec tests

--- config/initializers/build_version.rb
+++ config/initializers/build_version.rb
@@ -1,3 +1,5 @@
+# This file is created in the deployed container through the appeals deployment repo
+# https://github.com/department-of-veterans-affairs/appeals-deployment/blob/master/ansible/utility-roles/rails-build/templates/build_version.yml
 file_path = Rails.root + "config/build_version.yml"
 raw_config = File.exist?(file_path) ? File.read(file_path) : ""
 config = YAML.load(raw_config)
