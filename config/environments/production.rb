e83b79bd746da78518e40a2594246621a9bc45d6
Chris-Martine <christopher.martine@va.gov>
2024-02-14 12:25:34 -0500

    Change ssl_option exclusion route for health-check in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
-  config.ssl_options = { redirect: { exclude: -> request { request.path =~ /up/ } } }
+  config.ssl_options = { redirect: { exclude: -> request { request.path =~ /health-check/ } } }
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
