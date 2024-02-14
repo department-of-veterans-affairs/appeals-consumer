11cef2a973554c75ce853505ad8415532a8ebb3c
Eli Brown <elilogbro@gmail.com>
2024-02-14 14:20:34 -0500

    eli/Appeals-36704: Add method definitions to the ClaimReviewBuilder class (#33)
    * added method definitions
    
    * added spec
    
    * added decision review created factory traits

--- app/models/builders/claim_review_builder.rb
+++ app/models/builders/claim_review_builder.rb
@@ -4,6 +4,11 @@
 class Builders::ClaimReviewBuilder
   attr_reader :claim_review, :decision_review_created
 
+  PENSION_IDENTIFIER = "PMC"
+  FILED_BY_VA_GOV = false
+  COMPENSATION_BENEFIT_TYPE = "compensation"
+  PENSION_BENEFIT_TYPE = "pension"
+
   def self.build(decision_review_created)
     builder = new(decision_review_created)
     builder.assign_attributes
@@ -19,38 +24,71 @@ class Builders::ClaimReviewBuilder
     calculate_benefit_type
     assign_filed_by_va_gov
     assign_receipt_date
-    assign_legacy_opt_in_approved
+    calculate_legacy_opt_in_approved
     calculate_veteran_is_not_claimant
-    calculate_establishment_attempted_at
-    calculate_establishment_last_submitted_at
-    calculate_establishment_processed_at
-    calculate_establishment_submitted_at
+    assign_establishment_attempted_at
+    assign_establishment_last_submitted_at
+    assign_establishment_processed_at
+    assign_establishment_submitted_at
     assign_informal_conference
     assign_same_office
   end
 
   private
 
-  def calculate_benefit_type; end
+  def calculate_benefit_type
+    @claim_review.benefit_type = determine_benefit_type
+  end
 
-  # always false
-  def assign_filed_by_va_gov; end
+  def assign_filed_by_va_gov
+    @claim_review.filed_by_va_gov = FILED_BY_VA_GOV
+  end
 
-  def assign_receipt_date; end
+  def assign_receipt_date
+    @claim_review.receipt_date = decision_review_created.claim_received_date
+  end
 
-  def assign_legacy_opt_in_approved; end
+  def calculate_legacy_opt_in_approved
+    @claim_review.legacy_opt_in_approved = any_opted_in_issues?
+  end
 
-  def calculate_veteran_is_not_claimant; end
+  def calculate_veteran_is_not_claimant
+    @claim_review.veteran_is_not_claimant = veteran_and_claimant_participant_ids_different?
+  end
 
-  def calculate_establishment_attempted_at; end
+  def assign_establishment_attempted_at
+    @claim_review.establishment_attempted_at = decision_review_created.claim_creation_time
+  end
 
-  def calculate_establishment_last_submitted_at; end
+  def assign_establishment_last_submitted_at
+    @claim_review.establishment_last_submitted_at = decision_review_created.claim_creation_time
+  end
 
-  def calculate_establishment_processed_at; end
+  def assign_establishment_processed_at
+    @claim_review.establishment_processed_at = decision_review_created.claim_creation_time
+  end
 
-  def calculate_establishment_submitted_at; end
+  def assign_establishment_submitted_at
+    @claim_review.establishment_submitted_at = decision_review_created.claim_creation_time
+  end
 
-  def assign_informal_conference; end
+  def assign_informal_conference
+    @claim_review.informal_conference = decision_review_created.informal_conference_requested
+  end
 
-  def assign_same_office; end
+  def assign_same_office
+    @claim_review.same_office = decision_review_created.same_station_review_requested
+  end
+
+  def determine_benefit_type
+    decision_review_created.ep_code.include?(PENSION_IDENTIFIER) ? PENSION_BENEFIT_TYPE : COMPENSATION_BENEFIT_TYPE
+  end
+
+  def any_opted_in_issues?
+    decision_review_created.decision_review_issues.any? { |issue| !!issue.soc_opt_in }
+  end
+
+  def veteran_and_claimant_participant_ids_different?
+    decision_review_created.veteran_participant_id != decision_review_created.claimant_participant_id
+  end
 end
--- spec/models/builders/claim_review_builder_spec.rb
+++ spec/models/builders/claim_review_builder_spec.rb
@@ -32,16 +32,130 @@ describe Builders::ClaimReviewBuilder do
       expect(builder).to receive(:calculate_benefit_type)
       expect(builder).to receive(:assign_filed_by_va_gov)
       expect(builder).to receive(:assign_receipt_date)
-      expect(builder).to receive(:assign_legacy_opt_in_approved)
+      expect(builder).to receive(:calculate_legacy_opt_in_approved)
       expect(builder).to receive(:calculate_veteran_is_not_claimant)
-      expect(builder).to receive(:calculate_establishment_attempted_at)
-      expect(builder).to receive(:calculate_establishment_last_submitted_at)
-      expect(builder).to receive(:calculate_establishment_processed_at)
-      expect(builder).to receive(:calculate_establishment_submitted_at)
+      expect(builder).to receive(:assign_establishment_attempted_at)
+      expect(builder).to receive(:assign_establishment_last_submitted_at)
+      expect(builder).to receive(:assign_establishment_processed_at)
+      expect(builder).to receive(:assign_establishment_submitted_at)
       expect(builder).to receive(:assign_informal_conference)
       expect(builder).to receive(:assign_same_office)
 
       builder.assign_attributes
+    end
+  end
+
+  describe "#calculate_benefit_type" do
+    subject { builder.send(:calculate_benefit_type) }
+
+    context "when decision_review_created.ep_code includes 'PMC'" do
+      let(:decision_review_created) { build(:decision_review_created, :pension) }
+      it "assigns the claim_review's benefit_type to 'pension'" do
+        expect(subject).to eq(Builders::ClaimReviewBuilder::PENSION_BENEFIT_TYPE)
+      end
+    end
+
+    context "when decision_review_created.ep_code DOES NOT include 'PMC'" do
+      it "assigns the claim_review's benefit_type to 'compensation'" do
+        expect(subject).to eq(Builders::ClaimReviewBuilder::COMPENSATION_BENEFIT_TYPE)
+      end
+    end
+  end
+
+  describe "#assign_filed_by_va_gov" do
+    subject { builder.send(:assign_filed_by_va_gov) }
+    it "always assigns claim_review's filed_by_va_gov to false" do
+      expect(subject).to eq(Builders::ClaimReviewBuilder::FILED_BY_VA_GOV)
+    end
+  end
+
+  describe "#assign_receipt_date" do
+    subject { builder.send(:assign_receipt_date) }
+    it "assigns claim_review's receipt_date to decision_review_created.claim_received_date" do
+      expect(subject).to eq(decision_review_created.claim_received_date)
+    end
+  end
+
+  describe "#calculate_legacy_opt_in_approved" do
+    subject { builder.send(:calculate_legacy_opt_in_approved) }
+
+    context "when decision_review_created contains a decision_review_issue with soc_opt_in: true" do
+      let(:decision_review_created) { build(:decision_review_created, :soc_opt_in) }
+
+      it "assigns claim_review's legacy_opt_in_approved to true" do
+        expect(subject).to eq(true)
+      end
+    end
+
+    context "when decision_review_created DOES NOT contain a decision_review_issue with soc_opt_in: true" do
+      it "assigns claim_review's legacy_opt_in_approved to false" do
+        expect(subject).to eq(false)
+      end
+    end
+  end
+
+  describe "#calculate_veteran_is_not_claimant" do
+    subject { builder.send(:calculate_veteran_is_not_claimant) }
+
+    context "when decision_review_created.veteran_participant_id is NOT the same as"\
+      " decision_review_created.claimant_participant_id" do
+      it "assigns claim_review's veteran_is_not_claimant to true" do
+        expect(subject).to eq true
+      end
+    end
+
+    context "when decision_review_created.veteran_participant_id is the same as"\
+      " decision_review_created.claimant_participant_id" do
+      let(:decision_review_created) { build(:decision_review_created, :veteran_is_claimant) }
+      it "assigns claim_review's veteran_is_not_claimant to true" do
+        expect(subject).to eq false
+      end
+    end
+  end
+
+  describe "#assign_establishment_attempted_at" do
+    subject { builder.send(:assign_establishment_attempted_at) }
+
+    it "assigns claim_review's establishment_attempted_at to decision_review_created.claim_creation_time" do
+      expect(subject).to eq(decision_review_created.claim_creation_time)
+    end
+  end
+
+  describe "#assign_establishment_last_submitted_at" do
+    subject { builder.send(:assign_establishment_last_submitted_at) }
+
+    it "assigns claim_review's establishment_last_submitted_at to decision_review_created.claim_creation_time" do
+      expect(subject).to eq(decision_review_created.claim_creation_time)
+    end
+  end
+
+  describe "#assign_establishment_processed_at" do
+    subject { builder.send(:assign_establishment_processed_at) }
+
+    it "assigns claim_review's establishment_processed_at to decision_review_created.claim_creation_time" do
+      expect(subject).to eq(decision_review_created.claim_creation_time)
+    end
+  end
+
+  describe "#assign_establishment_submitted_at" do
+    subject { builder.send(:assign_establishment_submitted_at) }
+
+    it "assigns claim_review's establishment_submitted_at to decision_review_created.claim_creation_time" do
+      expect(subject).to eq(decision_review_created.claim_creation_time)
+    end
+  end
+
+  describe "#assign_informal_conference" do
+    subject { builder.send(:assign_informal_conference) }
+    it "assigns claim_review's informal_conference to decision_reciew_created.informal_conference_requested" do
+      expect(subject).to eq(decision_review_created.informal_conference_requested)
+    end
+  end
+
+  describe "#assign_same_office" do
+    subject { builder.send(:assign_same_office) }
+    it "assigns claim_review's same_office to decision_reciew_created.same_station_review_requested" do
+      expect(subject).to eq(decision_review_created.same_station_review_requested)
     end
   end
 end
