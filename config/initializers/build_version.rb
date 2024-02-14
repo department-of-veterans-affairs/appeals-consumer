770c5d03efce303d67aef4394d5a4a5ba34d9c43
Chris-Martine <christopher.martine@va.gov>
2024-02-14 11:16:56 -0500

    Update build_version comment link

--- config/initializers/build_version.rb
+++ config/initializers/build_version.rb
@@ -1,5 +1,5 @@
 # This file is created in the deployed container through the appeals deployment repo
-# https://github.com/department-of-veterans-affairs/appeals-deployment/blob/master/ansible/utility-roles/rails-build/templates/build_version.yml
+# https://github.com/department-of-veterans-affairs/appeals-deployment/blob/12e11cb88c9eafd0cb65ee15d603e483902aebca/ansible/roles/caseflow-consumer/tasks/main.yml#L42
 file_path = Rails.root + "config/build_version.yml"
 raw_config = File.exist?(file_path) ? File.read(file_path) : ""
 config = YAML.load(raw_config)
