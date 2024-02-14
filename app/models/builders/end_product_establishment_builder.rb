4445782e7f66135dd450e3572df162cbebf8d179
nkirby-va <nicholas.kirby@va.gov>
2024-02-14 16:53:55 -0500

    add parent model builder

--- app/models/builders/end_product_establishment_builder.rb
+++ app/models/builders/end_product_establishment_builder.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # This class is used to build out an End Product Establishment object from an instance of DecisionReviewCreated
-class Builders::EndProductEstablishmentBuilder
+class Builders::EndProductEstablishmentBuilder < Builders::ModelBuilder
   attr_reader :end_product_establishment, :decision_review_created
 
   def self.build(decision_review_created)
@@ -11,10 +11,14 @@ class Builders::EndProductEstablishmentBuilder
   end
 
   def initialize(decision_review_created)
+    super()
     @decision_review_created = decision_review_created
     @end_product_establishment = EndProductEstablishment.new
+    #@veteran_bis_record = fetch_veteran_bis_record
+    #@limited_poa_hash = fetch_limited_poa
   end
 
+  # :reek:TooManyStatements
   def assign_attributes
     calculate_benefit_type_code
     assign_claim_date
@@ -23,39 +27,70 @@ class Builders::EndProductEstablishmentBuilder
     assign_payee_code
     calculate_limited_poa_access
     calculate_limited_poa_code
-    calculate_committed_at
-    calculate_established_at
-    calculate_last_synced_at
+    assign_committed_at
+    assign_established_at
+    assign_last_synced_at
     assign_synced_status
     assign_development_item_reference_id
     assign_reference_id
+    self
   end
 
   private
 
-  def calculate_benefit_type_code; end
+  def calculate_benefit_type_code
+    # @end_product_establishment.benefit_type_code = if @veteran_bis_record[:date_of_death]
+    #                                                  2
+    #                                                else
+    #                                                  1
+    #                                                end
+  end
 
-  def assign_claim_date; end
+  def assign_claim_date
+    @end_product_establishment.claim_date = @decision_review_created.claim_received_date
+  end
 
-  def assign_code; end
+  def assign_code
+    @end_product_establishment.code = @decision_review_created.ep_code
+  end
 
-  def assign_modifier; end
+  def assign_modifier
+    @end_product_establishment.modifier = @decision_review_created.modifier
+  end
 
-  def assign_payee_code; end
+  def assign_payee_code
+    @end_product_establishment.payee_code = @decision_review_created.payee_code
+  end
 
-  def calculate_limited_poa_access; end
+  def calculate_limited_poa_access
+    #@limited_poa_hash[claim_id][:limited_poa_access]
+  end
 
-  def calculate_limited_poa_code; end
+  def calculate_limited_poa_code
+    #@limited_poa_hash[claim_id][:limited_poa_code]
+  end
 
-  def calculate_committed_at; end
+  def assign_committed_at
+    @end_product_establishment.committed_at = @decision_review_created.claim_creation_time
+  end
 
-  def calculate_established_at; end
+  def assign_established_at
+    @end_product_establishment.established_at = @decision_review_created.claim_creation_time
+  end
 
-  def calculate_last_synced_at; end
+  def assign_last_synced_at
+    @end_product_establishment.last_synced_at = @decision_review_created.claim_creation_time
+  end
 
-  def assign_synced_status; end
+  def assign_synced_status
+    @end_product_establishment.synced_status = @decision_review_created.claim_lifecycle_status
+  end
 
-  def assign_development_item_reference_id; end
+  def assign_development_item_reference_id
+    #@end_product_establishment.development_item_reference_id = @decision_review_created.informal_conference_tracked_item_id
+  end
 
-  def assign_reference_id; end
+  def assign_reference_id
+    @end_product_establishment.reference_id = @decision_review_created.claim_id
+  end
 end
---
+++ app/models/builders/model_builder.rb
@@ -0,0 +1,25 @@
+class Builders::ModelBuilder
+
+  class AppealsConsumer::Error::BisVeteranNotFound; end
+
+  def fetch_veteran_bis_record
+    bis_record = BISService.new.fetch_veteran_info(decision_review_created.file_number)
+
+    # If the result is nil, the veteran wasn't found
+    # If the participant id is nil, that's another way of saying the veteran wasn't found
+    unless bis_record && bis_record[:ptcpnt_id]
+      fail AppealsConsumer::Error::BisVeteranNotFound, "DecisionReviewCreated file number"\
+     " #{decision_review_created.file_number} does not have a valid BIS record"
+    end
+
+    @bis_synced_at = Time.zone.now
+    bis_record
+  end
+
+  def fetch_limited_poa
+    return unless @decision_review_created
+
+    limited_poa = BISService.new.fetch_limited_poas_by_claim_ids(@decision_review_created.claim_id)
+    limited_poa ? limited_poa[claim_id] : nil
+  end
+end
