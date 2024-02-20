75cac8f1f0c164aa7d31950b8fef7513e371a886
nkirby-va <nicholas.kirby@va.gov>
2024-02-20 14:37:39 -0500

    guard clause and check instance variable instead for consistency

--- app/models/builders/model_builder.rb
+++ app/models/builders/model_builder.rb
@@ -3,7 +3,9 @@ class Builders::ModelBuilder
   class AppealsConsumer::Error::BisVeteranNotFound; end
 
   def fetch_veteran_bis_record
-    bis_record = BISService.new.fetch_veteran_info(decision_review_created.file_number)
+    return unless @decision_review_created
+
+    bis_record = BISService.new.fetch_veteran_info(@decision_review_created.file_number)
 
     # If the result is nil, the veteran wasn't found
     # If the participant id is nil, that's another way of saying the veteran wasn't found
