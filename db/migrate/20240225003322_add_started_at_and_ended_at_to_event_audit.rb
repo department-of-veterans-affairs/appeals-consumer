7d88ce1d0f548f09dc8750f277fc0c644e71f997
nkirby-va <nicholas.kirby@va.gov>
2024-02-28 14:52:26 -0500

    modify started_at and ended_at column comments

--- db/migrate/20240225003322_add_started_at_and_ended_at_to_event_audit.rb
+++ db/migrate/20240225003322_add_started_at_and_ended_at_to_event_audit.rb
@@ -1,6 +1,6 @@
 class AddStartedAtAndEndedAtToEventAudit < ActiveRecord::Migration[7.1]
   def change
-    add_column :event_audits, :started_at, :timestamp, comment: "The time that the event_audit started processing."
-    add_column :event_audits, :ended_at, :timestamp, comment: "The time that the event_audit finished processing."
+    add_column :event_audits, :started_at, :timestamp, comment: "The time that the event_audit starts processing an event"
+    add_column :event_audits, :ended_at, :timestamp, comment: "The time that the event_audit finishes processing an event"
   end
 end
