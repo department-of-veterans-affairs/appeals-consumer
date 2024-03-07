0334dba6cd5b9dd05e0d664a04e6a35dac62ee09
david-whitlatch <david.whitlatch@va.gov>
2024-03-07 16:54:28 -0500

    WIP: add additional tests to base event processing job spec

--- spec/jobs/base_event_processing_job_spec.rb
+++ spec/jobs/base_event_processing_job_spec.rb
@@ -29,8 +29,10 @@ RSpec.describe BaseEventProcessingJob, type: :job do
       subject { job.perform(event) }
 
       before do
-        allow(event).to receive(:process!).and_raise(StandardError.new)
         allow(Rails.logger).to receive(:error)
+        ClimateControl.modify MAX_ERRORS_FOR_FAILURE: "3" do
+          expect(event.state).to eq("error")
+        end
         subject
       end
 
@@ -39,9 +41,57 @@ RSpec.describe BaseEventProcessingJob, type: :job do
         expect(Rails.logger)
           .to have_received(:error)
           .with(/An error has occured while processing a job for the event with event_id/)
-        expect(event.reload.state).to eq("error")
         expect(EventAudit.last.status).to eq("failed")
         expect(EventAudit.last.ended_at).not_to be_nil
+      end
+
+      context "outside of a transaction block" do
+        before do
+          allow(event).to receive(:process!).and_raise(StandardError.new)
+        end
+
+        context "when the number of event audits is less than MAX_ERRORS_FOR_FAILURE" do
+          ClimateControl.modify MAX_ERRORS_FOR_FAILURE: "3" do
+            expect(event.state).to eq("error")
+          end
+          expect(event.reload.state).to eq("error")
+        end
+
+        context "when the number of event audits is greater than or equal to MAX_ERRORS_FOR_FAILURE" do
+          ClimateControl.modify MAX_ERRORS_FOR_FAILURE: "3" do
+            expect(event.state).to eq("error")
+          end
+        end
+      end
+
+      context "within the start processing transaction" do
+        it "rollsback the transaction" do
+          allow_any_instance_of(EventAudit).to receive(:started_at!).and_raise(StandardError.new)
+
+          expect { subject }.not_to(change { EventAudit.count })
+          expect(event.reload.state).not_to eq("in_progress")
+        end
+      end
+
+      context "within the completed processing transaction" do
+        it "rollsback the transaction" do
+          allow_any_instance_of(EventAudit).to receive(:completed!).and_raise(StandardError.new)
+
+          expect(EventAudit.last.status).not_to eq("completed")
+          expect(event.reload.state).not_to eq("processed")
+        end
+      end
+
+      context "within the handle job error transaction" do
+        before do
+          allow(event).to receive(:failed!).and_raise(StandardError.new)
+        end
+
+        it "rollsback the transaction" do
+          allow_any_instance_of(EventAudit).to receive(:started_at!).and_raise(StandardError.new)
+          expect(EventAudit.last.status).not_to eq("failed")
+          expect(event.reload.state).not_to eq("failed")
+        end
       end
     end
   end
