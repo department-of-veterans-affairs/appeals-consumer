85d53a0393ce0d73c614ade627ab07afdd9dd4d0
Chris-Martine <135330019+Chris-Martine@users.noreply.github.com>
2024-03-11 09:36:09 -0400

    cmartine/APPEALS-40993 (#48)
    * Set config.colorize_logging to false
    
    * Add redis store to config.cache_store in application.rb
    
    * Fix redis env var and url in cable.yml
    
    * Fix linter errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,9 @@ Rails.application.configure do
   # want to log everything, set the level to "debug".
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
+  # Don't colorize logging in production (for easier to read log files)
+  config.colorize_logging = false
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
