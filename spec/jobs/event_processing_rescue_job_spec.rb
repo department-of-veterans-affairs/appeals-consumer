c280b981f51c9149c8389fbf4f6b52cade71f449
david-whitlatch <david.whitlatch@va.gov>
2024-03-19 17:16:51 -0400

    change spec for event processing rescue job

--- spec/jobs/event_processing_rescue_job_spec.rb
+++ spec/jobs/event_processing_rescue_job_spec.rb
@@ -19,6 +19,7 @@ describe EventProcessingRescueJob, type: :job do
       let!(:stuck_audit) do
         create(:event_audit, :stuck, event: event)
       end
+
       it "processes stuck audits" do
         expect { EventProcessingRescueJob.perform_now }
           .to change { stuck_audit.reload.status }.from("in_progress").to("cancelled")
@@ -42,24 +43,59 @@ describe EventProcessingRescueJob, type: :job do
         expect(Rails.logger)
           .to receive(:info)
           .with("[EventProcessingRescueJob] Time limit exceeded, stopping job execution.")
-        EventProcessingRescueJob.perform_now
+        EventProcessingRescueJob.new.perform
         expect(stuck_audit.reload.status).not_to eq("cancelled")
       end
     end
   end
 
+  describe "rescue_from StandardError" do
+    it "logs an error and handles the exception" do
+      allow(EventAudit).to receive(:stuck).and_raise(StandardError)
+      expect(Rails.logger)
+        .to receive(:error)
+        .with(/\[EventProcessingRescueJob\] encountered an exception:/)
+      expect { EventProcessingRescueJob.perform_now }.not_to raise_error
+    end
+  end
+
   describe "private methods" do
+    let!(:event) { create(:decision_review_created_event) }
     let!(:stuck_audit) do
-      create(:event_audit, :stuck, event: create(:decision_review_created_event))
+      create(:event_audit, :stuck, event: event)
     end
 
     describe "#_process_audit" do
-      it "logs an error and handles the exception" do
-        allow(stuck_audit).to receive(:cancelled!).and_raise(StandardError.new("Test Error"))
-        expect(Rails.logger)
-          .to receive(:error)
-          .with(/\[EventProcessingRescueJob\] Failed to process audit/)
-        expect { EventProcessingRescueJob.new.send(:process_audit, stuck_audit) }.not_to raise_error
+      context "when an error is raised" do
+        it "logs an error and handles the exception" do
+          allow(stuck_audit).to receive(:cancelled!).and_raise(StandardError.new("Test Error"))
+          expect(Rails.logger)
+            .to receive(:error)
+            .with(/\[EventProcessingRescueJob\] Failed to process audit/)
+          expect { EventProcessingRescueJob.new.send(:process_audit, stuck_audit) }.not_to raise_error
+        end
+      end
+    end
+
+    describe "#_handle_reenqueue" do
+      context "when an error is raised" do
+        it "logs an error and handles the exception" do
+          allow(event).to receive(:determine_job).and_raise(StandardError.new("Test Error"))
+          expect(Rails.logger)
+            .to receive(:error)
+            .with(/\[EventProcessingRescueJob\] Error during the re-enqueue for event/)
+          expect { EventProcessingRescueJob.new.send(:handle_reenqueue, event) }.not_to raise_error
+        end
+      end
+
+      context "the determine_job return 'nil'" do
+        it "logs an error" do
+          allow(event).to receive(:determine_job).and_return(nil)
+          expect(Rails.logger)
+            .to receive(:error)
+            .with(/\[EventProcessingRescueJob\] Failed to re-enqueue job for Event ID:/)
+          EventProcessingRescueJob.new.send(:handle_reenqueue, event)
+        end
       end
     end
   end
