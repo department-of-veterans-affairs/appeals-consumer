b44c4f11b3f587e6bc723c2a2c62d9bf6301de0c
Matt Ray <matthew.ray@va.gov>
2024-04-01 12:43:54 -0500

    enable debugging

--- karafka.rb
+++ karafka.rb
@@ -10,7 +10,8 @@ class KarafkaApp < Karafka::App
         'sasl.username': ENV["KAFKA_USERNAME"],
         'sasl.password': ENV["KAFKA_PASSWORD"],
         'sasl.mechanisms': "PLAIN",
-        'group.id': "APPEALS_APL_SERVICES_UAT"
+        'group.id': "APPEALS_APL_SERVICES_UAT",
+        'debug': "all"
       }
     end
     config.kafka = karafka_config
