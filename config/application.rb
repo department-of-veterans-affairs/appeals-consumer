a56b9073bfe4a758a72824a040eda062df41f4b2
david-whitlatch <david.whitlatch@va.gov>
2024-04-05 11:40:58 -0400

    change caseflow url config to config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -50,6 +50,8 @@ module AppealsConsumer
     config.station_id = "283"
     config.css_id = "APPEALSCONSUMER1"
     config.redis_url = ENV["REDIS_URL_CACHE"]
+    config.caseflow_url = ENV["CASEFLOW_URL"]
+    config.caseflow_key = ENV["CASEFLOW_KEY"]
 
     config.cache_store = :redis_cache_store, { url: ENV["REDIS_URL_CACHE"], expires_in: 24.hours }
 
