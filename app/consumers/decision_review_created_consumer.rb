ff8c87b56af0089f95b0eda6591aa95578b02df5
Griffin Dooley <60793223+gcd253@users.noreply.github.com>
2024-07-26 16:45:43 -0400

    Decision Review Updated Consumer and Consumer Group (#103)
    * consumer and consumer group
    
    * changed confusing variable name
    
    * rewording and renaming items
    
    * renaming in rspec for continuity
    
    * refactored ApplicationConsumer
    
    * reverting to previous commit
    
    * method names in consumer changed to be specific
    
    * drc and dru consumers updated with AppConsumer methods
    
    * revisions based on code review

--- app/consumers/decision_review_created_consumer.rb
+++ app/consumers/decision_review_created_consumer.rb
@@ -5,7 +5,7 @@
 class DecisionReviewCreatedConsumer < ApplicationConsumer
   include LoggerMixin
   # Defines the event type string for decision review created events to standardize the event handling process.
-  EVENT_TYPE = "Events::DecisionReviewCreatedEvent"
+  EVENT_TYPE = Events::DecisionReviewCreatedEvent
 
   # Consumes messages from the Kafka topic, processing each message to handle event creation,
   # job enqueueing and error management. It iterates over each message, logging the start and end of
@@ -16,13 +16,15 @@ class DecisionReviewCreatedConsumer < ApplicationConsumer
                           service: :kafka,
                           name: "DecisionReviewCreatedConsumer.consume") do
       messages.each do |message|
-        extra_details = extra_details(message)
+        claim_id = message.payload.message["claim_id"]
+        extra_details = extra_details(message, EVENT_TYPE.to_s,
+                                      consumer_specific_details: { claim_id: claim_id })
 
         log_consumption_start(extra_details)
 
         begin
           ActiveRecord::Base.transaction do
-            event = handle_event_creation(message)
+            event = handle_event_creation(message, EVENT_TYPE)
 
             # Processes the event with additional logic provided in the block for job enqueueing.
             process_event(event, extra_details) do |new_event|
@@ -32,7 +34,7 @@ class DecisionReviewCreatedConsumer < ApplicationConsumer
           end
         rescue StandardError => error
           if attempt > 3
-            logger.error(error, sentry_details(message), notify_alerts: true)
+            logger.error(error, sentry_details(message, EVENT_TYPE.to_s), notify_alerts: true)
             next
           else
             logger.error(error, extra_details)
@@ -45,39 +47,4 @@ class DecisionReviewCreatedConsumer < ApplicationConsumer
     end
   end
   # rubocop:enable Metrics/MethodLength
-
-  private
-
-  # Attempts to find or initialize a new decision review created event based on message metadata.
-  # This method ensures that each event is uniquely identified by its poartition and offset,
-  # preventing duplicate processing of the same event.
-  def handle_event_creation(message)
-    Events::DecisionReviewCreatedEvent.find_or_initialize_by(
-      partition: message.metadata.partition,
-      offset: message.metadata.offset
-    ) do |event|
-      event.type = EVENT_TYPE
-      event.message_payload = message.payload.message
-    end
-  end
-
-  # Extracts and returns a hash of extra details from the message for logging and diagnostic purposes.
-  # These details include the kafka partition, offset, and the claim ID extracted from the message payload.
-  def extra_details(message)
-    {
-      type: EVENT_TYPE,
-      partition: message.metadata.partition,
-      offset: message.metadata.offset,
-      claim_id: message.payload.message["claim_id"]
-    }
-  end
-
-  def sentry_details(message)
-    {
-      type: EVENT_TYPE,
-      partition: message.metadata.partition,
-      offset: message.metadata.offset,
-      message_payload: message.payload.message
-    }
-  end
 end
