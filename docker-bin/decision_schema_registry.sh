c120138191c10c08652054f9dfd1f3c1d08228cc
(Jeffrey) Aaron Willis <98484567+Aaron-Willis@users.noreply.github.com>
2024-08-19 09:02:30 -0400

    APPEALS-54019 Added Decision Avro and Updated schema_registry shell scripts.

---
+++ docker-bin/decision_schema_registry.sh
@@ -0,0 +1,18 @@
+export TOPIC=BIA_SERVICES_BIE_CATALOG_LOCAL_DECISION_V01
+
+jq -Rs '{ schema: .}' /usr/bin/$TOPIC.avsc > temp-schema.json
+
+if [ ! -f temp-schema.json ]; then
+  echo "Failed to create temp schema file"
+  exit 1
+fi
+
+curl -s -X POST \
+  -H "Content-Type:application/vnd.schemaregistry.v1+json" \
+  --data @temp-schema.json http://schema-registry:9021/subjects/$TOPIC/versions > /dev/null
+
+echo "Decision AVRO was uploaded to the Schema-registry successfully"
+
+export SCHEMA_VERSION=$(curl -s -X GET http://schema-registry:9021/subjects/$TOPIC/versions/latest | jq '.version')
+
+rm temp-schema.json
