a2018e24b7d5803859fda2ae4eef0c99752ef6a1
Nico Rithner <nicolas.rithner@va.gov>
2024-08-20 12:30:39 -0600

    Migrate all methods to base class

--- app/models/builders/base_request_issue_builder.rb
+++ app/models/builders/base_request_issue_builder.rb
@@ -2,6 +2,21 @@
 
 class Builders::BaseRequestIssueBuilder
   include DecisionReview::ModelBuilder
+
+  # returns the DecisionReviewCreated::RequestIssue record with all attributes assigned
+  def self.build(issue, decision_review_model, bis_rating_profiles)
+    builder = new(issue, decision_review_model, bis_rating_profiles)
+    builder.assign_attributes
+    builder.request_issue
+  end
+
+  def initialize(issue, decision_review_model, bis_rating_profiles)
+    @decision_review_model = decision_review_model
+    @issue = issue
+    @bis_rating_profiles = bis_rating_profiles
+    @request_issue = DecisionReviewCreated::RequestIssue.new
+  end
+
   REQUEST_ISSUE = "RequestIssue"
   NONRATING_EP_CODE_CATEGORY = "NON_RATING"
 
@@ -45,6 +60,47 @@ class Builders::BaseRequestIssueBuilder
     "683SCRRRAMP" => "Supplemental Claim Review Rating",
     "683HAERRAMP" => "Higher-Level Review Additional Evidence Rating"
   }.freeze
+
+  # -- Methods
+
+  def assign_attributes
+    assign_methods
+    calculate_methods
+  end
+
+  private
+
+  def assign_methods
+    assign_contested_rating_decision_reference_id
+    assign_contested_rating_issue_reference_id
+    assign_contested_decision_issue_id
+    assign_untimely_exemption
+    assign_untimely_exemption_notes
+    assign_vacols_id
+    assign_vacols_sequence_id
+    assign_nonrating_issue_bgs_id
+    assign_type
+  end
+
+  def calculate_methods
+    calculate_contention_reference_id
+    calculate_benefit_type
+    calculate_contested_issue_description
+    calculate_contested_rating_issue_profile_date
+    calculate_decision_date
+    calculate_ineligible_due_to_id
+    calculate_ineligible_reason
+    calculate_unidentified_issue_text
+    calculate_nonrating_issue_category
+    calculate_nonrating_issue_description
+    calculate_closed_at
+    calculate_closed_status
+    calculate_contested_rating_issue_diagnostic_code
+    calculate_rating_issue_associated_at
+    calculate_ramp_claim_id
+    calculate_is_unidentified
+    calculate_nonrating_issue_bgs_source
+  end
 
   # EP codes ending in "PMC" are pension, otherwise "compensation"
   def calculate_benefit_type
@@ -448,5 +504,19 @@ class Builders::BaseRequestIssueBuilder
   def handle_missing_decision_date
     msg = "Issue with contention_id #{issue.contention_id} is identified but has null for prior_decision_date"
     fail AppealsConsumer::Error::NullPriorDecisionDate, msg
+  end
+
+  # used to determine if "TIME_RESTRICTION" eligibility_result maps to "untimely" or "before_ama" ineligible_reason
+  def decision_date_before_ama?
+    decision_date = prior_decision_date_converted_to_logical_type
+
+    if decision_date
+      ama_activation_date_logical_type = (AMA_ACTIVATION_DATE - EPOCH_DATE).to_i
+      decision_date < ama_activation_date_logical_type
+    end
+  end
+
+  def determine_benefit_type
+    decision_review_model.ep_code.include?(PENSION_IDENTIFIER) ? PENSION_BENEFIT_TYPE : COMPENSATION_BENEFIT_TYPE
   end
 end
