607f70be52b8095e85d13e93b1130df407deca75
Griffin Dooley <60793223+gcd253@users.noreply.github.com>
2024-08-22 14:52:47 -0400

    Create Decision Review Updated End Product Establishment Builder (#124)
    * builder and model created
    
    * spec files (wip)
    
    * rspec tests do not rely on factory
    
    * integrate rspec shared context
    
    * update language to "decision_review_model" in epe and claim review builders

---
+++ app/models/builders/decision_review_updated/end_product_establishment_builder.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# This class is used to build out an End Product Establishment object from an instance of DecisionReviewUpdated
+class Builders::DecisionReviewUpdated::EndProductEstablishmentBuilder
+  attr_reader :end_product_establishment, :decision_review_model
+
+  def self.build(decision_review_model)
+    builder = new(decision_review_model)
+    builder.assign_attributes
+    builder.end_product_establishment
+  end
+
+  def initialize(decision_review_model)
+    @decision_review_model = decision_review_model
+    @end_product_establishment = DecisionReviewUpdated::EndProductEstablishment.new
+  end
+
+  def assign_attributes
+    assign_decision_review_updated_development_item_reference_id
+    assign_decision_review_updated_reference_id
+    self
+  end
+
+  private
+
+  def assign_decision_review_updated_development_item_reference_id
+    @end_product_establishment.development_item_reference_id =
+      @decision_review_model.informal_conference_tracked_item_id
+  end
+
+  def assign_decision_review_updated_reference_id
+    @end_product_establishment.reference_id = @decision_review_model.claim_id.to_s
+  end
+end
