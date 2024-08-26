f7db7efc91a2b0ff3c9007c1641068b0e31028bf
Griffin Dooley <dooleygriffin@gmail.com>
2024-08-26 13:03:55 -0400

    request issue builder (wip)

---
+++ app/models/builders/decision_review_updated/request_issue_builder.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# This class is used to build out an individual Request Issue from a DecisionReviewUpdated Event
+class Builders::DecisionReviewUpdated::RequestIssueBuilder
+    attr_reader :decision_review_model, :issue, :request_issue
+
+    def initialize(issue, decision_review_model, bis_rating_profiles)
+      @request_issue = DecisionReviewCreated::RequestIssue.new
+      super(issue, decision_review_model, bis_rating_profiles, @request_issue)
+    end
+end
