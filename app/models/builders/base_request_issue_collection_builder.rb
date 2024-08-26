55a69821fbe701a7d09997779e263316107b3e0e
Chris-Martine <135330019+Chris-Martine@users.noreply.github.com>
2024-08-26 12:58:02 -0400

    cmartine/APPEALS-53198 (#119)
    * Move RequestIssueCollectionBuilder methods up to super class BaseRequestIssueCollectionBuilder
    
    * Edit BaseRequestIssueCollectionBuilder include name
    
    * Change name of class in failed to build messages
    
    * Edit BaseRequestIssueCollectionBuilder include name again
    
    * Rename decision_review_created to decision_review_model in BaseRequestIssueCollectionBuilder and spec
    
    * Change error messages to match subclass name
    
    * Change directory name from DecisionReviewCreated to DecisionReview
    
    * Create Parent class BaseRequestIssueBuilder
    
    * Inherit from parent class and move constants
    
    Inherit from BaseRequestIssueBuilder
    Move constants to parent class
    
    * Migrate batch of method from request_issue_builder
    
    Test still pass after migrating this batch
    
    * Migrate 2nd batch of method from request_issue_builder
    
    Test still pass after migrating this batch
    
    * Migrate 3rd batch of method from request_issue_builder
    
    Test still pass after migrating this batch
    Except decision_date_before_ama? method.
    No passing method not migrated yet
    
    * Migrate 4rd batch of method from request_issue_builder
    
    Test still pass after migrating this batch
    
    * Migrate 5rd batch of method from request_issue_builder
    
    Test still pass after migrating this batch
    Except determine_benefit_type method.
    No passing method not migrated yet
    
    * Revert model_builder changes
    
    * Migrate 6rd final batch of method from request_issue_builder
    
    Test still pass after migrating this batch
    Except determine_benefit_type method.
    Except decision_date_before_ama?
    Lat two not migrated
    
    * Include DecisionReview::ModelBuilder into BaseRequestIssueBuilder
    
    * Changed decision_review_created to model. Tests pass
    
    * Changed decision_review_created to model in request_issue_builder
    
    model_builder.rb
    base_request_issue_builder.rb
    
    * Roll back name change
    
    Change back to decision_review_created from
    decision_review_model
    All tests pass
    
    * Update to decision_review_model
    
    * Add method wrapper to utilize @decision_review_created as decision_review_model
    
    * Change names from decision_review_created to decision_review_model
    
    * Add @decision_review_model to decision_review_model helper method
    
    * Add explicit return nil
    
    * Correct linting errors
    
    * Migrate all methods to base class
    
    * Implement build_issues and build_request_issue in Builders::DecisionReviewCreated::RequestIssueCollectionBuilder
    
    * Remove wrapper method
    
    Remove wrapper method decision_review_model
    and replace every instance of decision_review_model
    with @decision_review_model
    
    * Update to work with @decision_review_model
    
    * Update request_issue_collection_builder to work with @decision_review_model
    
    * Change variable name in builders specs
    
    From @decision_review_created to
    @decision_review_model
    All test pass
    
    * Remove commented out code from builders
    
    * Change name to ModelBuilderHelper in BaseRequestIssueCollectionBuilder
    
    * Refactor model_builder to model_builder_helper
    
    * Move creation of request issue model
    
    Add initialize method to child class
    Instantiate DecisionReviewCreated request issue
    in child class.
    Update parameters
    
    * Update intake_builder to use decision_review_model
    
    * Update claim_review_builder to use decision_review_model
    
    * Update comment to reflect name change
    
    * Fix linter errors
    
    ---------
    
    Co-authored-by: Nico Rithner <nicolas.rithner@va.gov>

---
+++ app/models/builders/base_request_issue_collection_builder.rb
@@ -0,0 +1,108 @@
+# frozen_string_literal: true
+
+class Builders::BaseRequestIssueCollectionBuilder
+  include DecisionReview::ModelBuilderHelper
+  # issues with this eligibility_result are not included in the caseflow payload
+  # caseflow does not track or have a concept of this when determining ineligible_reason
+  CONTESTED = "CONTESTED"
+  RATING = "RATING"
+
+  def self.build(decision_review_model)
+    builder = new(decision_review_model)
+    builder.build_issues
+  end
+
+  # only fetch BIS rating profiles if there are rating issues
+  def initialize(decision_review_model)
+    @decision_review_model = decision_review_model
+    @bis_rating_profiles = nil
+
+    # only fetch and set BIS rating profiles if the message is rating and contains an identified BIS rating issue
+    if message_has_rating_issues?
+      initialize_issue_profile_dates
+      fetch_and_set_bis_rating_profiles
+    end
+  end
+
+  def build_issues
+    fail NotImplementedError, "#{self.class} must implement the build_issues method"
+  end
+
+  # used to call BIS fetch_rating_profiles_in_range with specified date range
+  # one day is added to the latest date incase a rating profile was added today
+  def initialize_issue_profile_dates
+    @earliest_issue_profile_date = earliest_issue_profile_date
+    @latest_issue_profile_date_plus_one_day = latest_issue_profile_date_plus_one_day
+  end
+
+  # if issue profile date variables aren't nil,
+  # fetch rating profiles from BIS and overwrite @bis_rating_profiles with response
+  def fetch_and_set_bis_rating_profiles
+    if @earliest_issue_profile_date && @latest_issue_profile_date_plus_one_day
+      @bis_rating_profiles = fetch_bis_rating_profiles
+    end
+  end
+
+  private
+
+  # exception thrown if there aren't any issues after removing issues with "CONTESTED" eligibility_result
+  def valid_issues
+    valid_issues = remove_ineligible_contested_issues
+    handle_no_issues_after_removing_contested if valid_issues.empty?
+
+    valid_issues
+  end
+
+  def message_has_rating_issues?
+    rating_ep_code_category? && at_least_one_valid_bis_issue?
+  end
+
+  def rating_ep_code_category?
+    @decision_review_model.ep_code_category.upcase == RATING
+  end
+
+  def at_least_one_valid_bis_issue?
+    valid_issues.any?(&:prior_rating_decision_id)
+  end
+
+  def remove_ineligible_contested_issues
+    @decision_review_model.decision_review_issues.reject { |issue| issue.eligibility_result == CONTESTED }
+  end
+
+  def handle_no_issues_after_removing_contested
+    fail AppealsConsumer::Error::RequestIssueCollectionBuildError, "Failed building from"\
+      " #{self.class} for DecisionReview Claim ID:"\
+      " #{@decision_review_model.claim_id} does not contain any valid issues after"\
+      " removing 'CONTESTED' ineligible issues"
+  end
+
+  def build_request_issue(issue, index)
+    fail NotImplementedError, "#{self.class} must implement the build__request_issue method"
+  end
+
+  # in cases where the decision review issue has null for contention_id, use the index of the issue as the identifier
+  def issue_identifier_message(issue, index)
+    (!!issue.contention_id) ? "Issue Contention ID: #{issue.contention_id}" : "Issue Index: #{index}"
+  end
+
+  def earliest_issue_profile_date
+    valid_issue_profile_dates&.min
+  end
+
+  def latest_issue_profile_date
+    valid_issue_profile_dates&.max
+  end
+
+  def latest_issue_profile_date_plus_one_day
+    latest_issue_profile_date + 1 if !!latest_issue_profile_date
+  end
+
+  def valid_issue_profile_dates
+    # no need to include invalid issue profile dates since they won't be included in the caseflow payload
+    profile_dates = valid_issues.map(&:prior_decision_rating_profile_date)
+    return nil if profile_dates.all?(&:nil?)
+
+    # unidentified issues can have nil for this field, so remove nil values before mapping
+    profile_dates.compact.map(&:to_date)
+  end
+end
