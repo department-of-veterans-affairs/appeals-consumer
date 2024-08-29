730a467fc15c9f9ef72be877f6d8caecc2e39423
Chris-Martine <christopher.martine@va.gov>
2024-08-29 08:13:25 -0400

    Add BaseRequestIssueCollectionBuilder inheritance to RemovedIssueCollectionBuilder, make empty build_issues build_request_issue methods

--- app/models/builders/decision_review_updated/removed_issue_collection_builder.rb
+++ app/models/builders/decision_review_updated/removed_issue_collection_builder.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
-class Builders::DecisionReviewUpdated::RemovedIssueCollectionBuilder
-  def self.build(decision_review_updated); end
+class Builders::DecisionReviewUpdated::RemovedIssueCollectionBuilder < Builders::BaseRequestIssueCollectionBuilder
+  def build_issues
+  
+  end
+
+  def build_request_issue
+    
+  end
 end
