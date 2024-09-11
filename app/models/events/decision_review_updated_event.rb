6e96a7f16c33c7ea0b702cd736cc1db710e591f8
Christian Lazo <lazochristian.cl@gmail.com>
2024-09-10 17:42:17 -0700

    Change max error count, add logic for decision review event pending

--- app/models/events/decision_review_updated_event.rb
+++ app/models/events/decision_review_updated_event.rb
@@ -2,5 +2,22 @@
 
 # A subclass of Event, representing the DecisionReviewUpdated Kafka topic event.
 class Events::DecisionReviewUpdatedEvent < Event
-  def process!; end
+  def process!
+    fail AppealsConsumer::Error::PreviousDecisionReviewEventsStillPending if decision_review_event_pending?
+  end
+
+  # WIP: This may change, not in love with this solution just yet
+  def decision_review_event_pending?
+    Events::DecisionReviewCreatedEvent
+      .where(claim_id: event.claim_id)
+      .where.not(state: "processed")
+      .where(completed_at: nil)
+      .exists? ||
+      Events::DecisionReviewUpdatedEvent
+        .where(claim_id: event.claim_id)
+        .where("update_time < ?", current_event.update_time)
+        .where.not(state: "processed")
+        .where(completed_at: nil)
+        .exists?
+  end
 end
