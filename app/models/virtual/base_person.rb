efb1c849eae7f5c8c264ab595c3e46093f05891c
Dani <danielle.coleman1@va.gov>
2024-10-09 15:57:38 -0400

    adds file number and date of death attributes

--- app/models/virtual/base_person.rb
+++ app/models/virtual/base_person.rb
@@ -2,5 +2,5 @@
 
 class BasePerson
   attr_accessor :participant_id, :name_suffix, :ssn, :first_name, :middle_name, :last_name,
-                :email_address, :date_of_birth
+                :email_address, :date_of_birth, :date_of_death, :file_number
 end
