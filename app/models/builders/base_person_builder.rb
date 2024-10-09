e1d76ca41286b9e13a8d55bff197527d51e19875
Dani <danielle.coleman1@va.gov>
2024-10-09 15:54:04 -0400

    adds file number and date of death attributes

--- app/models/builders/base_person_builder.rb
+++ app/models/builders/base_person_builder.rb
@@ -23,6 +23,8 @@ class Builders::BasePersonBuilder
     calculate_ssn
     calculate_date_of_birth
     calculate_email_address
+    calculate_date_of_death
+    assign_file_number
     # assign_veteran_indicator <-- should this be an attribute?
   end
 
@@ -58,6 +60,15 @@ class Builders::BasePersonBuilder
 
   def calculate_email_address
     @person.email_address = @person_updated_model.email_address
+  end
+
+  # this date is sort of hard codded in the person factory
+  def calculate_date_of_death
+    @person.date_of_death = @person_updated_model.date_of_death if @person_updated_model.date_of_death 
+  end
+
+  def assign_file_number
+    @person.file_number = @person_updated_model.file_number
   end
 
   # def assign_veteran_indicator; end
