78903049a80ac5cf1ea0616132153683fa84f059
Dani <danielle.coleman1@va.gov>
2024-10-11 14:16:58 -0400

    updated person_builder file name to dto_builder

--- app/models/builders/person_updated/person_builder.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Builders::PersonUpdated::PersonBuilder < Builders::BasePersonBuilder
-  def initialize(person_updated_model)
-    @person = PersonUpdated::Person.new
-    super
-  end
-end
