8c56a80cb8f71ae9451a17022bdb6aca1cfdf55e
Dani <danielle.coleman1@va.gov>
2024-10-11 13:36:18 -0400

    adds is_veteran attribute

--- spec/models/virtual/base_person_spec.rb
+++ spec/models/virtual/base_person_spec.rb
@@ -12,7 +12,8 @@ describe BasePerson do
     expect(person).to respond_to(:middle_name)
     expect(person).to respond_to(:last_name)
     expect(person).to respond_to(:email_address)
-    expect(person).to respond_to(:file_number)
     expect(person).to respond_to(:date_of_death)
+    expect(person).to respond_to(:file_number)
+    expect(person).to respond_to(:is_veteran)
   end
 end
