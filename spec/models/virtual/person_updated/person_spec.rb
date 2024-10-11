2ed14567b48620b8b11489812d7032e8a8d84b16
Dani <danielle.coleman1@va.gov>
2024-10-11 13:42:08 -0400

    adds is_veteran attribute

--- spec/models/virtual/person_updated/person_spec.rb
+++ spec/models/virtual/person_updated/person_spec.rb
@@ -11,6 +11,9 @@ describe PersonUpdated::Person do
     expect(person.middle_name).to eq("Russell")
     expect(person.last_name).to eq("Smith")
     expect(person.email_address).to eq("email@email.com")
-    expect(person.date_of_birth).to eq(Date.new(2022, 1, 1))
+    expect(person.date_of_birth).to eq(Date.new(1969, 1, 1))
+    expect(person.date_of_death).to eq(Date.new(2022, 1, 1))
+    expect(person.file_number).to eq("123456789")
+    expect(person.is_veteran).to eq(false)
   end
 end
