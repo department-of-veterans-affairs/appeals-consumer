8d1ad13a41922cfeecd71a264cf0322548d44a1a
Dani <danielle.coleman1@va.gov>
2024-10-11 13:40:30 -0400

    add is_veteran attribute set to false and veteran trait is_veteran set to true

--- spec/factories/person.rb
+++ spec/factories/person.rb
@@ -10,18 +10,15 @@ FactoryBot.define do
     middle_name { "Russell" }
     last_name { "Smith" }
     email_address { "email@email.com" }
-    date_of_birth { Date.new(2022, 1, 1) }
-    date_of_death { Date.new(2018, 1, 1) }
+    date_of_birth { Date.new(1969, 1, 1) }
+    date_of_death { Date.new(2022, 1, 1) }
     file_number { "123456789" }
+    is_veteran { false }
 
     # do we need date of birth?
 
-    # Will I need the trait?
-    # trait :without_bis_attrs do
-    #   # date_of_death { nil }
-    #   name_suffix { nil }
-    #   ssn { nil }
-    #   middle_name { nil }
-    # end
+    trait :veteran do
+      is_veteran { true }
+    end
   end
 end
