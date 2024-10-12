233d18d0f6aebbb713225aed66c18002240168d4
raymond-hughes <raymond.hughes2@va.gov>
2024-10-11 23:58:43 -0400

    Rename BIA schema file and refactor update_person schema registry file

--- docker-bin/person_updated_schema_registry.sh
+++ docker-bin/person_updated_schema_registry.sh
@@ -1,4 +1,4 @@
-export TOPIC=BIA_SERVICES_BIE_CATALOG_LOCAL_PERSON_UPDATED_V01
+export TOPIC=BIA_SERVICES_BIE_CATALOG_LOCAL_PARTICIPANT_PERSON_UPDATED_V01
 
 jq -Rs '{ schema: .}' /usr/bin/$TOPIC.avsc > temp-schema.json
 
@@ -7,26 +7,12 @@ if [ ! -f temp-schema.json ]; then
   exit 1
 fi
 
-jq '
-  . + {
-    0,
-    references: [
-      {
-        "name": "gov.va.bip.bie.participant.person",
-        "subject": "BIA_SERVICES_BIE_CATALOG_LOCAL_PARTICIPANT_PERSON_UPDATED_V01",
-        "version": 1
-      }
-    ]
-  }
-' temp-schema.json > temp-schema-with-references.json
-
-response=$(curl -s -X POST \
+curl -s -X POST \
   -H "Content-Type:application/vnd.schemaregistry.v1+json" \
-  --data @temp-schema-with-references.json \
-  http://schema-registry:9021/subjects/$TOPIC/versions)
+  --data @temp-schema.json http://schema-registry:9021/subjects/$TOPIC/versions > /dev/null
 
 echo "PersonUpdated AVRO was uploaded to the Schema-registry successfully"
 
-export SCHEMA_VERSION_UPDATED=$(curl -s -X GET http://schema-registry:9021/subjects/$TOPIC/versions/latest | jq '.version')
+export SCHEMA_VERSION=$(curl -s -X GET http://schema-registry:9021/subjects/$TOPIC/versions/latest | jq '.version')
 
-rm temp-schema.json temp-schema-with-references.json
+rm temp-schema.json
