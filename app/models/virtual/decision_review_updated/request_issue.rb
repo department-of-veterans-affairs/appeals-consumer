cc82a1808a6bf9f7bcacdfbc423e1646a5af53cf
(Jeffrey) Aaron Willis <98484567+Aaron-Willis@users.noreply.github.com>
2024-10-13 02:02:30 -0400

    Revert "Feature/appeals 49673 (#160)" (#162)
    This reverts commit af29ea776348ca5a727ad847e6e4562ee5eb5195.

--- app/models/virtual/decision_review_updated/request_issue.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# This class should be instantiated via Builders::DecisionReviewUpdeted::RequestIssueBuilder
-class DecisionReviewUpdated::RequestIssue < BaseRequestIssue
-  attr_accessor :edited_description, :original_caseflow_request_issue_id
-end
