3699e286c8f33b59171bd3c292942dcd01bbb27f
raymond-hughes <131811099+raymond-hughes@users.noreply.github.com>
2024-10-17 15:35:33 -0400

    Transformers: Updating virtual model transformers to use NilClass and serialized attributes. Payload Validator: Fixed warning:Lint/ShadowingOuterLocalVariable that was present in validate_attribute_data_types method Person Updated Context: Reconfigured attributes to be formatted correctly

--- app/models/virtual/transformers/person_updated.rb
+++ app/models/virtual/transformers/person_updated.rb
@@ -7,16 +7,16 @@ class Transformers::PersonUpdated
   attr_reader :event_id
 
   PERSON_UPDATED_ATTRIBUTES = {
-    "participant_id" => Integer,
-    "name_suffix" => String,
-    "ssn" => String,
-    "first_name" => String,
-    "middle_name" => String,
-    "last_name" => String,
-    "email_address" => String,
-    "date_of_birth" => Integer,
-    "date_of_death" => Integer,
-    "file_number" => String,
+    "participant_id" => String,
+    "name_suffix" => [String, NilClass],
+    "ssn" => [String, NilClass],
+    "first_name" => [String, NilClass],
+    "middle_name" => [String, NilClass],
+    "last_name" => [String, NilClass],
+    "email_address" => [String, NilClass],
+    "date_of_birth" => [String, NilClass],
+    "date_of_death" => [String, NilClass],
+    "file_number" => [String, NilClass],
     "is_veteran" => [TrueClass, FalseClass]
   }.freeze
   # Allows read and write access for attributes
@@ -26,6 +26,7 @@ class Transformers::PersonUpdated
   # presence, attribute names and data types, assign the incoming attributes to defined keys,
   # and create PersonUpdated instances for each object in message_payload's decision_review_issues array
   def initialize(event_id, message_payload)
+    puts message_payload
     @event_id = event_id
     validate(message_payload, self.class.name)
     assign(message_payload)
@@ -41,5 +42,8 @@ class Transformers::PersonUpdated
     PERSON_UPDATED_ATTRIBUTES.each_key do |attr|
       instance_variable_set("@#{attr}", message_payload[attr])
     end
+  end
+
+  class Long < Integer
   end
 end
