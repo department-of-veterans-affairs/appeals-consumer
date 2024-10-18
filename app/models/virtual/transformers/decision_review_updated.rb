9c029a16645291c5652992400c2c174ebf034339
raymond-hughes <131811099+raymond-hughes@users.noreply.github.com>
2024-10-17 22:18:15 -0400

    Fixing merge conflicts

--- app/models/virtual/transformers/decision_review_updated.rb
+++ app/models/virtual/transformers/decision_review_updated.rb
@@ -189,17 +189,6 @@ class Decision
   DECISION_ATTRIBUTES = {
     "contention_id" => Integer,
     "disposition" => String,
-<<<<<<< HEAD
-    "dta_error_explanation" => String,
-    "decision_source" => String,
-    "category" => String,
-    "decision_id" => Integer,
-    "decision_text" => String,
-    "award_event_id" => Integer,
-    "rating_profile_date" => String,
-    "decision_recorded_time" => String,
-    "decision_finalized_time" => String
-=======
     "dta_error_explanation" => [String, NilClass],
     "decision_source" => [String, NilClass],
     "category" => [String, NilClass],
@@ -209,7 +198,6 @@ class Decision
     "rating_profile_date" => [String, NilClass],
     "decision_recorded_time" => String,
     "decision_finalized_time" => [String, NilClass]
->>>>>>> main
   }.freeze
 
   DECISION_ATTRIBUTES.each_key { |attr_name| attr_accessor attr_name }
