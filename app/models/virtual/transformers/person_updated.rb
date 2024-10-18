0a95b8d45ee41876b53ebc230db98922ba094f6a
Dani Coleman <60626984+dcoleman21@users.noreply.github.com>
2024-10-18 09:54:11 -0400

    Dani/APPEALS-59863 (#171)
    * adds route for person_updated_cosumer_group
    
    * adds person_udpated transformer class and methods
    
    * adds person_updated transformer tests
    
    * fix typo on line 64
    
    * updated is_veteran attribute to an array with TrueClass, FalseClass
    
    * Raymond/APPEALS-58630 (#157)
    
    * Adding new person updated event model with stubbed functionallity
    
    * Adding tests for new person updated event model
    
    * Adding spec for avro schema testing and new fasctory for person event
    
    * Adding person updated avsc schema files
    
    * Update rubocop to no longer track NumericLiterals
    
    * Update person updated schema registry with temp name field until we determine the correct naming for person updated event schema
    
    * Adding person updated to Makefile
    
    * Rename BIA schema file and refactor update_person schema registry file
    
    * Adding update_person to docker-bin startup shell script to allow docker to pick up update_person schema registry
    
    * Rename BIA Docker bin file
    
    * Remove event file added by mistake
    
    * Updating docker-bin person updated schema registry script
    
    * Fixing linting issue
    
    * Trigger Build
    
    * Update echo to show PersonUpdated version
    
    * Remove duplicate person updated event
    
    * Adding the complete schema file for person_updated
    
    * Reset person updated registry schema with correct permissions
    
    * Adding person updated env var and updating person updated schema registry config
    
    * Update config comment
    
    * Adding person updated event file back as it was removed in error
    
    * adds reference to shared context file for message payload
    
    * add a context file to use in person updated spec
    
    * Adding convention:Rails/Date to rubocop ignore as this is a false positive per https://stackoverflow.com/questions/15038726/ruby-on-rails-convention-for-naming-date-time-fields
    
    * Transformers: Updating virtual model transformers to use NilClass and serialized attributes.
    Payload Validator: Fixed warning:Lint/ShadowingOuterLocalVariable that was present in validate_attribute_data_types method
    Person Updated Context: Reconfigured attributes to be formatted correctly
    
    * updates tests all passing
    
    * rebase
    
    * Dani/APPEALS-59864 Consumer: Create PersonUpdatedEventProcessingJob (#181)
    
    * adds person updated event processing job class
    
    * adds person updated event processing job class
    
    * adds person updated event processing job spec with supporting tests
    
    * add Final newline lint correction
    
    * Raymond/APPEALS-58630 (#157)
    
    * Adding new person updated event model with stubbed functionallity
    
    * Adding tests for new person updated event model
    
    * Adding spec for avro schema testing and new fasctory for person event
    
    * Adding person updated avsc schema files
    
    * Update rubocop to no longer track NumericLiterals
    
    * Update person updated schema registry with temp name field until we determine the correct naming for person updated event schema
    
    * Adding person updated to Makefile
    
    * Rename BIA schema file and refactor update_person schema registry file
    
    * Adding update_person to docker-bin startup shell script to allow docker to pick up update_person schema registry
    
    * Rename BIA Docker bin file
    
    * Remove event file added by mistake
    
    * Updating docker-bin person updated schema registry script
    
    * Fixing linting issue
    
    * Trigger Build
    
    * Update echo to show PersonUpdated version
    
    * Remove duplicate person updated event
    
    * Adding the complete schema file for person_updated
    
    * Reset person updated registry schema with correct permissions
    
    * Adding person updated env var and updating person updated schema registry config
    
    * Update config comment
    
    * updates person updated event factory to have expected attributes
    
    * remove puts statement in initialize method
    
    ---------
    
    Co-authored-by: Raymond Hughes <131811099+raymond-hughes@users.noreply.github.com>
    Co-authored-by: raymond-hughes <raymond.hughes2@va.gov>

---
+++ app/models/virtual/transformers/person_updated.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+# PersonUpdated represents the message_payload from a PersonUpdatedEvent
+class Transformers::PersonUpdated
+  include MessagePayloadValidator
+
+  attr_reader :event_id
+
+  PERSON_UPDATED_ATTRIBUTES = {
+    "participant_id" => String,
+    "name_suffix" => [String, NilClass],
+    "ssn" => [String, NilClass],
+    "first_name" => [String, NilClass],
+    "middle_name" => [String, NilClass],
+    "last_name" => [String, NilClass],
+    "email_address" => [String, NilClass],
+    "date_of_birth" => [String, NilClass],
+    "date_of_death" => [String, NilClass],
+    "file_number" => [String, NilClass],
+    "is_veteran" => [TrueClass, FalseClass]
+  }.freeze
+  # Allows read and write access for attributes
+  PERSON_UPDATED_ATTRIBUTES.each_key { |attr_name| attr_accessor attr_name }
+
+  # When PersonUpdated.new(message_payload) is called, this method will validate message_payload
+  # presence, attribute names and data types, assign the incoming attributes to defined keys,
+  # and create PersonUpdated instances for each object in message_payload's decision_review_issues array
+  def initialize(event_id, message_payload)
+    @event_id = event_id
+    validate(message_payload, self.class.name)
+    assign(message_payload)
+  end
+
+  # Lists the attributes and corresponding data types
+  def attribute_types
+    PERSON_UPDATED_ATTRIBUTES
+  end
+
+  # Assigns attributes from the message_payload to defined keys
+  def assign(message_payload)
+    PERSON_UPDATED_ATTRIBUTES.each_key do |attr|
+      instance_variable_set("@#{attr}", message_payload[attr])
+    end
+  end
+
+  class Long < Integer
+  end
+end
