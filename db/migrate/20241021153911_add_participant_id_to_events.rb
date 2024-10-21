9584032082328f26d615b28e17f94cd86a5fb9ee
Noelle Adkin <98478937+NoelleAd@users.noreply.github.com>
2024-10-21 12:33:06 -0400

    disable ddl transaction and update schema file.

--- db/migrate/20241021153911_add_participant_id_to_events.rb
+++ db/migrate/20241021153911_add_participant_id_to_events.rb
@@ -1,4 +1,6 @@
 class AddParticipantIdToEvents < ActiveRecord::Migration[7.1]
+  disable_ddl_transaction!
+
   def up
     add_column :events, :participant_id, :integer, comment: "Participant ID associated with People Updated Events"
     add_index :events, :participant_id, algorithm: :concurrently
