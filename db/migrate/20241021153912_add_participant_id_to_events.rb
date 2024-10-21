149cdf9f73cc4c36b493680b8353af077f601f40
Noelle Adkin <98478937+NoelleAd@users.noreply.github.com>
2024-10-21 14:23:46 -0400

    APPEALS-60891 (#187)
    * Add Migration file.
    
    * fix typo on participant_id
    
    * disable ddl transaction and update schema file.
    
    * update from int to string. Increment timestamp.

---
+++ db/migrate/20241021153912_add_participant_id_to_events.rb
@@ -0,0 +1,13 @@
+class AddParticipantIdToEvents < ActiveRecord::Migration[7.1]
+  disable_ddl_transaction!
+
+  def up
+    add_column :events, :participant_id, :string, comment: "Participant ID associated with People Updated Events"
+    add_index :events, :participant_id, algorithm: :concurrently
+  end
+
+  def down
+    remove_column :events, :participant_id, :string, comment: "Participant ID associated with People Updated Events"
+    remove_index :events, :participant_id, algorithm: :concurrently
+  end
+end
