048c2b826bd02f87c088bd01bec8f2f2cb572ef1
Noelle Adkin <98478937+NoelleAd@users.noreply.github.com>
2024-10-21 12:21:03 -0400

    Add Migration file.

---
+++ db/migrate/20241021153911_add_participant_id_to_events.rb
@@ -0,0 +1,11 @@
+class AddParticipantIdToEvents < ActiveRecord::Migration[7.1]
+  def up
+    add_column :events, :participant_id, :integer, comment: "Participant ID associated with People Updated Events"
+    add_index :events, participant_id, algorithm: :concurrently
+  end
+
+  def down
+    remove_column :events, :participant_id, :integer, comment: "Participant ID associated with People Updated Events"
+    remove_index :events, :participant_id, algorithm: :concurrently
+  end
+end
