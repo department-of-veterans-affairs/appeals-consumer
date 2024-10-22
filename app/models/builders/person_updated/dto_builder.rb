4e309ad22689ea2943ce867a2bc506d71b28e948
Dani <60626984+dcoleman21@users.noreply.github.com>
2024-10-22 17:15:24 -0400

    updates instance variables for attributes

--- app/models/builders/person_updated/dto_builder.rb
+++ app/models/builders/person_updated/dto_builder.rb
@@ -31,47 +31,47 @@ class Builders::PersonUpdated::DtoBuilder
   private
 
   def assign_first_name
-    @person_updated.first_name = @person_updated_event.first_name
+    @person.first_name = @person_updated.first_name
   end
 
   def assign_last_name
-    @person_updated.last_name = @person_updated_event.last_name
+    @person.last_name = @person_updated.last_name
   end
 
   def calculate_middle_name
-    @person_updated.middle_name = @person_updated_event.middle_name
+    @person.middle_name = @person_updated.middle_name
   end
 
   def calculate_name_suffix
-    @person_updaed.name_suffix = @person_updated_event.name_suffix
+    @person.name_suffix = @person_updated.name_suffix
   end
 
   def assign_participant_id
-    @person_updated.participant_id = @person_updated_event.participant_id
+    @person.participant_id = @person_updated.participant_id
   end
 
   def calculate_ssn
-    @person_updated.ssn = @person_updated_event.ssn
+    @person.ssn = @person_updated.ssn
   end
 
   def calculate_date_of_birth
-    @person_udated.date_of_birth = @person_updated_event.date_of_birth if @person_updated_event.date_of_birth
+    @person.date_of_birth = @person_updated.date_of_birth if @person_updated.date_of_birth
   end
 
   def calculate_email_address
-    @person_updated.email_address = @person_updated_event.email_address
+    @person.email_address = @person_updated.email_address
   end
 
   def calculate_date_of_death
-    @person_updated.date_of_death = @person_updated_event.date_of_death if @person_updated_event.date_of_death
+    @person.date_of_death = @person_updated.date_of_death if @person_updated.date_of_death
   end
 
   def assign_file_number
-    @person_updated.file_number = @person_updated_event.file_number
+    @person.file_number = @person_updated.file_number
   end
 
   def assign_veteran_indicator
-    @person_updated.is_veteran = @person_updated_event.is_veteran
+    @person.is_veteran = @person_updated.is_veteran
   end
 
   def build_person_updated(message_payload)
