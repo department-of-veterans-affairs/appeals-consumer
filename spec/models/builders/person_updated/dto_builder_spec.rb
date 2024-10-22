abd289c8a2f5edce18496159cabf10bba02d6d2d
Raymond Hughes <131811099+raymond-hughes@users.noreply.github.com>
2024-10-22 15:59:52 -0400

    Fixing DTO Builder test so it picks up the correct Event

--- spec/models/builders/person_updated/dto_builder_spec.rb
+++ spec/models/builders/person_updated/dto_builder_spec.rb
@@ -1,24 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.describe Builders::PersonUpdated::DtoBuilder, type: :model do
-  message_payload = {
-    "date_of_birth" => Date.new(1980, 1, 1).to_time.to_s,
-    "date_of_death" => Date.new(2022, 1, 1).to_time.to_s,
-    "file_number" => "123456789",
-    "first_name" => "Bill",
-    "last_name" => "Tester",
-    "middle_name" => "T",
-    "participant_id" => "987654321",
-    "prefix" => "Mr",
-    "ssn" => "834295567",
-    "name_suffix" => nil,
-    "is_veteran" => true
-  }
-  # let(:person_updated_event) do
-  #   create(:person_updated_event, type: "Events::PersonUpdatedEvent")
-  # end
-  # let(:dto_builder) { described_class.new(person_updated_event) }
-  # let(:event_id) { person_updated_event.id }
+  let(:person_updated_event) do
+    create(:person_updated_event, type: "Events::PersonUpdatedEvent")
+  end
+
+  let(:dto_builder) { described_class.new(person_updated_event) }
+
+  let(:event_id) { person_updated_event.id }
 
   describe "initialize" do
     context "when a person_updated object is found from a mocked payload (before actually building)" do
@@ -41,7 +30,7 @@ RSpec.describe Builders::PersonUpdated::DtoBuilder, type: :model do
       end
     end
 
-    xit "initializes instance variables" do
+    it "initializes instance variables" do
       expect(dto_builder.instance_variable_get(:@person_updated)).to be_a(Transformers::PersonUpdated)
     end
   end
