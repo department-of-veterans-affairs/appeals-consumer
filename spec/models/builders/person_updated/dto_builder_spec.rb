b051b2edd03575a66b06b5c0bbe403b5d45d2e95
Noelle Adkin <98478937+NoelleAd@users.noreply.github.com>
2024-10-23 12:13:43 -0400

    Update DTO builder ans Spec

--- spec/models/builders/person_updated/dto_builder_spec.rb
+++ spec/models/builders/person_updated/dto_builder_spec.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.describe Builders::PersonUpdated::DtoBuilder, type: :model do
-  let(:dto_builder) { described_class.new(person_updated_event) }
   let(:person_updated_event) do
-    create(:event, type: "Events::PersonUpdatedEvent")
+    create(:person_updated_event, type: "Events::PersonUpdatedEvent")
   end
+
+  let(:dto_builder) { described_class.new(person_updated_event) }
   let(:event_id) { person_updated_event.id }
+  let(:person) { described_class.new(person) }
 
   describe "initialize" do
     it "calls MetricsService to record metrics" do
@@ -19,24 +21,47 @@ RSpec.describe Builders::PersonUpdated::DtoBuilder, type: :model do
     end
 
     it "initializes instance variables" do
-      expect(dto_builder.instance_variable_get(:@participant_id)).to eq(participant_id)
+      expect(dto_builder.instance_variable_get(:@event_id)).to be_a(Integer)
       expect(dto_builder.instance_variable_get(:@person_updated)).to be_a(Transformers::PersonUpdated)
+      expect(dto_builder.instance_variable_get(:@person)).to be_a(BasePerson)
     end
   end
 
   describe "#assign_attributes" do
     it "correctly assigns attributes" do
-      expect(dto_builder.first_name).to eq(person_updated_event.first_name)
-      expect(dto_builder.last_name).to eq(person_updated_event.last_name)
-      expect(dto_builder.middle_name).to eq(person_updated_event.middle_name)
-      expect(dto_builder.name_suffix).to eq(person_updated_event.name_suffix)
-      expect(dto_builder.participant_id).to eq(person_updated_event.participant_id)
-      expect(dto_builder.ssn).to eq(person_updated_event.ssn)
-      expect(dto_builder.date_of_birth).to eq(person_updated_event.date_of_birth)
-      expect(dto_builder.email_address).to eq(person_updated_event.email_address)
-      expect(dto_builder.date_of_death).to eq(person_updated_event.date_of_death)
-      expect(dto_builder.file_number).to eq(person_updated_event.file_number)
-      expect(dto_builder.is_veteran).to eq(person_updated_event.is_veteran)
+      expect(dto_builder.instance_variable_get(:@person_updated).first_name).to eq(
+        dto_builder.instance_variable_get(:@person).first_name
+      )
+      expect(dto_builder.instance_variable_get(:@person_updated).last_name).to eq(
+        dto_builder.instance_variable_get(:@person).last_name
+      )
+      expect(dto_builder.instance_variable_get(:@person_updated).middle_name).to eq(
+        dto_builder.instance_variable_get(:@person).middle_name
+      )
+      expect(dto_builder.instance_variable_get(:@person_updated).name_suffix).to eq(
+        dto_builder.instance_variable_get(:@person).name_suffix
+      )
+      expect(dto_builder.instance_variable_get(:@person_updated).participant_id).to eq(
+        dto_builder.instance_variable_get(:@person).participant_id
+      )
+      expect(dto_builder.instance_variable_get(:@person_updated).ssn).to eq(
+        dto_builder.instance_variable_get(:@person).ssn
+      )
+      expect(dto_builder.instance_variable_get(:@person_updated).date_of_birth).to eq(
+        dto_builder.instance_variable_get(:@person).date_of_birth
+      )
+      expect(dto_builder.instance_variable_get(:@person_updated).email_address).to eq(
+        dto_builder.instance_variable_get(:@person).email_address
+      )
+      expect(dto_builder.instance_variable_get(:@person_updated).date_of_death).to eq(
+        dto_builder.instance_variable_get(:@person).date_of_death
+      )
+      expect(dto_builder.instance_variable_get(:@person_updated).file_number).to eq(
+        dto_builder.instance_variable_get(:@person).file_number
+      )
+      expect(dto_builder.instance_variable_get(:@person_updated).is_veteran).to eq(
+        dto_builder.instance_variable_get(:@person).is_veteran
+      )
     end
   end
 end
