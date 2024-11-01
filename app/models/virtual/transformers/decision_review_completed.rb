244df226f17121edc2a60358d6518c373178f695
Griffin Dooley <60793223+gcd253@users.noreply.github.com>
2024-11-01 14:08:21 -0400

    Build DecisionReviewCompleted Plain Old Ruby Object Transformer (#196)
    * wip
    
    * first draft
    
    * moved code for clarity
    
    * fixed issue where decision could be empty hash
    
    * assign method for transformer made private
    
    * added code comments
    
    * finalize attribute data types
    
    * changed conflicting class name
    
    * added three sad path scenarios
    
    * addressed comments

---
+++ app/models/virtual/transformers/decision_review_completed.rb
@@ -0,0 +1,177 @@
+# frozen_string_literal: true
+
+# DecisionReviewCompleted represents the message_payload from an individual DecisionReviewCompletedEvent
+class Transformers::DecisionReviewCompleted
+  include MessagePayloadValidator
+
+  attr_reader :event_id
+
+  # Lists the attributes and corresponding data types
+  # Data types are listed in an array when the value isn't limited to one data type
+  # For example, remand_created could be a boolean OR nil
+  DECISION_REVIEW_COMPLETED_ATTRIBUTES = {
+    "claim_id" => Integer,
+    "decision_review_type" => [String, NilClass],
+    "veteran_participant_id" => [String, NilClass],
+    "claimant_participant_id" => [String, NilClass],
+    "remand_created" => [TrueClass, FalseClass, NilClass],
+    "ep_code_category" => [String, NilClass],
+    "claim_lifecycle_status" => [String, NilClass],
+    "actor_username" => [String, NilClass],
+    "actor_application" => [String, NilClass],
+    "completion_time" => [String, NilClass],
+    "decision_review_issues_completed" => Array
+  }.freeze
+  # Allows read and write access for attributes
+  DECISION_REVIEW_COMPLETED_ATTRIBUTES.each_key { |attr_name| attr_accessor attr_name }
+
+  # When DecisionReviewCompleted.new(message_payload) is called, this method will validate message_payload
+  # presence, attribute names and data types, assign the incoming attributes to defined keys,
+  # and create DecisionReviewIssueCompleted instances for each object in
+  # message_payload's decision_review_issues_completed array
+  def initialize(event_id, message_payload)
+    @event_id = event_id
+    validate(message_payload, self.class.name)
+    assign(message_payload)
+  end
+
+  # Lists the attributes and corresponding data types
+  def attribute_types
+    DECISION_REVIEW_COMPLETED_ATTRIBUTES
+  end
+
+  private
+
+  # Assigns attributes from the message_payload to defined keys
+  def assign(message_payload)
+    attribute_types.each_key do |attr|
+      instance_variable_set("@#{attr}", message_payload[attr])
+    end
+
+    decision_review_issues_completed_array = message_payload["decision_review_issues_completed"]
+    @decision_review_issues_completed = create_decision_review_issues_completed(decision_review_issues_completed_array)
+  end
+
+  # Creates instances of DecisionReviewIssueCompleted, validates attributes, and assigns attributes
+  # for each object in the decision_review_issues_completed array
+  # Fails out of workflow if decision_review_issues_completed is an empty array
+  def create_decision_review_issues_completed(decision_review_issues_completed)
+    if decision_review_issues_completed.blank?
+      fail ArgumentError,
+           "#{self.class.name}: Message payload must include at least one decision review issue completed"
+    end
+
+    decision_review_issues_completed.map { |issue| DecisionReviewIssueCompleted.new(issue) }
+  end
+end
+
+# DecisionReviewIssueCompleted represents an individual issue
+# object from the message_payload's decision_review_issues_completed
+class DecisionReviewIssueCompleted
+  include MessagePayloadValidator
+
+  # Lists the attributes and corresponding data types
+  # Data types are stored in an array when the value isn't limited to one data type
+  # For example, soc_opt_in could be a boolean OR nil
+  DECISION_REVIEW_ISSUE_COMPLETED_ATTRIBUTES = {
+    "decision_review_issue_id" => Integer,
+    "contention_id" => [Integer, NilClass],
+    "remand_claim_id" => [String, NilClass],
+    "remand_contention_id" => [String, NilClass],
+    "unidentified" => [TrueClass, FalseClass, NilClass],
+    "prior_rating_decision_id" => [Integer, NilClass],
+    "prior_non_rating_decision_id" => [Integer, NilClass],
+    "prior_caseflow_decision_issue_id" => [Integer, NilClass],
+    "prior_decision_rating_sn" => [String, NilClass],
+    "prior_decision_text" => [String, NilClass],
+    "prior_decision_type" => [String, NilClass],
+    "prior_decision_source" => [String, NilClass],
+    "prior_decision_notification_date" => [String, NilClass],
+    "legacy_appeal_issue_id" => [Integer, NilClass],
+    "prior_decision_rating_profile_date" => [String, NilClass],
+    "soc_opt_in" => [TrueClass, FalseClass, NilClass],
+    "legacy_appeal_id" => [String, NilClass],
+    "decision" => [Hash, NilClass]
+  }.freeze
+  # Allows read and write access for attributes
+  DECISION_REVIEW_ISSUE_COMPLETED_ATTRIBUTES.each_key { |attr_name| attr_accessor attr_name }
+
+  # When DecisionReviewIssueCompleted.new(issue_attrs) is called, this method will validate message_payload
+  # presence, attribute names and data types and assign the incoming attributes to defined keys
+  def initialize(issue = {})
+    validate(issue, self.class.name)
+    assign(issue)
+    create_decisions(issue["decision"])
+  end
+
+  # Lists the attributes and corresponding data types
+  def attribute_types
+    DECISION_REVIEW_ISSUE_COMPLETED_ATTRIBUTES
+  end
+
+  private
+
+  # Assigns attributes from issue_attrs to defined keys
+  def assign(issue)
+    attribute_types.each_key do |attr|
+      instance_variable_set("@#{attr}", issue[attr])
+    end
+  end
+
+  def create_decisions(decision)
+    return if decision.nil?
+
+    @decision = CompletedDecision.new(decision)
+  end
+end
+
+# CompletedDecision represents an individual decision object from
+# a decision_review_issues_completed's decision field
+class CompletedDecision
+  include MessagePayloadValidator
+
+  DECISION_ATTRIBUTES = {
+    "contention_id" => Integer,
+    "disposition" => String,
+    "dta_error_explanation" => [String, NilClass],
+    "decision_source" => [String, NilClass],
+    "category" => [String, NilClass],
+    "decision_id" => [Integer, NilClass],
+    "decision_text" => [String, NilClass],
+    "award_event_id" => [Integer, NilClass],
+    "rating_profile_date" => [String, NilClass],
+    "decision_recorded_time" => String,
+    "decision_finalized_time" => [String, NilClass]
+  }.freeze
+
+  DECISION_ATTRIBUTES.each_key { |attr_name| attr_accessor attr_name }
+
+  # When CompletedDecision.new(decision) is called, this method will validate message_payload
+  # presence, attribute names and data types and assign the incoming attributes to defined keys
+  def initialize(decision = {})
+    validate(decision, self.class.name)
+    assign(decision)
+  end
+
+  # Lists the attributes and corresponding data types
+  def attribute_types
+    DECISION_ATTRIBUTES
+  end
+
+  private
+
+  # Assigns attributes from issue_attrs to defined keys
+  def assign(decision)
+    attribute_types.each_key do |attr|
+      instance_variable_set("@#{attr}", decision[attr])
+    end
+  end
+
+  # Overwrites validate_presence method in MessagePayloadValidator because a
+  # DecisionReviewCompleted CompletedDecision CAN be nil but not an empty hash
+  def validate_presence(decision, class_name)
+    if decision.blank?
+      fail ArgumentError, "#{class_name}: Message payload cannot be empty"
+    end
+  end
+end
---
+++ spec/models/virtual/transformers/decision_review_completed_spec.rb
@@ -0,0 +1,346 @@
+# frozen_string_literal: true
+
+require "shared_context/decision_review_completed_context"
+
+describe Transformers::DecisionReviewCompleted do
+  subject(:decision_review_completed) { described_class.new(event_id, message_payload) }
+
+  include_context "decision_review_completed_context"
+
+  let(:event_id) { 96 }
+  let(:drc_class) { "Transformers::DecisionReviewCompleted" }
+  let(:decision_review_issue_completed_class) { "DecisionReviewIssueCompleted" }
+  let(:decision_class) { "CompletedDecision" }
+
+  describe "#initialize" do
+    context "when Transformers::DecisionReviewCompleted, DecisionReviewIssueCompleted, and CompletedDecision" \
+    " portions of payload have valid attributes and data types" do
+      it "initializes a Transformers::DecisionReviewCompleted object" do
+        expect(subject.claim_id).to eq(message_payload["claim_id"])
+        expect(subject.decision_review_type).to eq(message_payload["decision_review_type"])
+        expect(subject.veteran_participant_id).to eq(message_payload["veteran_participant_id"])
+        expect(subject.claimant_participant_id).to eq(message_payload["claimant_participant_id"])
+        expect(subject.remand_created).to eq(message_payload["remand_created"])
+        expect(subject.ep_code_category).to eq(message_payload["ep_code_category"])
+        expect(subject.claim_lifecycle_status).to eq(message_payload["claim_lifecycle_status"])
+        expect(subject.actor_username).to eq(message_payload["actor_username"])
+        expect(subject.actor_application).to eq(message_payload["actor_application"])
+        expect(subject.completion_time).to eq(message_payload["completion_time"])
+        expect(subject.decision_review_issues_completed.size)
+          .to eq(message_payload["decision_review_issues_completed"].count)
+      end
+
+      it "initializes a DecisionReviewIssueCompleted object for every obj in decision_review_issues_completed array" do
+        subject.decision_review_issues_completed.each do |issue|
+          expect(issue).to be_an_instance_of(DecisionReviewIssueCompleted)
+
+          # this additional logic of checking the issues by contention_id enables us to not rely on the index each
+          # DecisionReviewIssueCompleted is stored at since the index order could change when Github Actions runs
+          case issue.contention_id
+          when 345_456_567
+            expect(issue.contention_id).to eq(completed_decision_review_issue["contention_id"])
+            expect(issue.decision_review_issue_id).to eq(completed_decision_review_issue["decision_review_issue_id"])
+            expect(issue.remand_claim_id).to eq(completed_decision_review_issue["remand_claim_id"])
+            expect(issue.remand_contention_id).to eq(completed_decision_review_issue["remand_contention_id"])
+            expect(issue.unidentified).to eq(completed_decision_review_issue["unidentified"])
+            expect(issue.prior_rating_decision_id).to eq(completed_decision_review_issue["prior_rating_decision_id"])
+            expect(issue.prior_non_rating_decision_id)
+              .to eq(completed_decision_review_issue["prior_non_rating_decision_id"])
+            expect(issue.prior_caseflow_decision_issue_id)
+              .to eq(completed_decision_review_issue["prior_caseflow_decision_issue_id"])
+            expect(issue.prior_decision_rating_sn).to eq(completed_decision_review_issue["prior_decision_rating_sn"])
+            expect(issue.prior_decision_text).to eq(completed_decision_review_issue["prior_decision_text"])
+            expect(issue.prior_decision_type).to eq(completed_decision_review_issue["prior_decision_type"])
+            expect(issue.prior_decision_source).to eq(completed_decision_review_issue["prior_decision_source"])
+            expect(issue.prior_decision_notification_date)
+              .to eq(completed_decision_review_issue["prior_decision_notification_date"])
+            expect(issue.legacy_appeal_issue_id).to eq(completed_decision_review_issue["legacy_appeal_issue_id"])
+            expect(issue.prior_decision_rating_profile_date)
+              .to eq(completed_decision_review_issue["prior_decision_rating_profile_date"])
+            expect(issue.soc_opt_in).to eq(completed_decision_review_issue["soc_opt_in"])
+            expect(issue.legacy_appeal_id).to eq(completed_decision_review_issue["legacy_appeal_id"])
+          when 987_876_765
+            expect(issue.contention_id).to eq(canceled_decision_review_issue["contention_id"])
+            expect(issue.decision_review_issue_id).to eq(canceled_decision_review_issue["decision_review_issue_id"])
+            expect(issue.remand_claim_id).to eq(canceled_decision_review_issue["remand_claim_id"])
+            expect(issue.remand_contention_id).to eq(canceled_decision_review_issue["remand_contention_id"])
+            expect(issue.unidentified).to eq(canceled_decision_review_issue["unidentified"])
+            expect(issue.prior_rating_decision_id).to eq(canceled_decision_review_issue["prior_rating_decision_id"])
+            expect(issue.prior_non_rating_decision_id)
+              .to eq(canceled_decision_review_issue["prior_non_rating_decision_id"])
+            expect(issue.prior_caseflow_decision_issue_id)
+              .to eq(canceled_decision_review_issue["prior_caseflow_decision_issue_id"])
+            expect(issue.prior_decision_rating_sn).to eq(canceled_decision_review_issue["prior_decision_rating_sn"])
+            expect(issue.prior_decision_text).to eq(canceled_decision_review_issue["prior_decision_text"])
+            expect(issue.prior_decision_type).to eq(canceled_decision_review_issue["prior_decision_type"])
+            expect(issue.prior_decision_source).to eq(canceled_decision_review_issue["prior_decision_source"])
+            expect(issue.prior_decision_notification_date)
+              .to eq(canceled_decision_review_issue["prior_decision_notification_date"])
+            expect(issue.legacy_appeal_issue_id).to eq(canceled_decision_review_issue["legacy_appeal_issue_id"])
+            expect(issue.prior_decision_rating_profile_date)
+              .to eq(canceled_decision_review_issue["prior_decision_rating_profile_date"])
+            expect(issue.soc_opt_in).to eq(canceled_decision_review_issue["soc_opt_in"])
+            expect(issue.legacy_appeal_id).to eq(canceled_decision_review_issue["legacy_appeal_id"])
+          end
+        end
+      end
+
+      it "initializes a CompletedDecision object for every DecisionReviewIssueCompleted"\
+      " with a non-nil decision field" do
+        subject.decision_review_issues_completed.each do |issue|
+          decision = issue.decision
+
+          case issue.contention_id
+          when 345_456_567
+            expect(decision).to be_an_instance_of(CompletedDecision)
+            expect(decision.contention_id).to eq(completed_decision["contention_id"])
+            expect(decision.disposition).to eq(completed_decision["disposition"])
+            expect(decision.dta_error_explanation).to eq(completed_decision["dta_error_explanation"])
+            expect(decision.decision_source).to eq(completed_decision["decision_source"])
+            expect(decision.category).to eq(completed_decision["category"])
+            expect(decision.decision_id).to eq(completed_decision["decision_id"])
+            expect(decision.decision_text).to eq(completed_decision["decision_text"])
+            expect(decision.award_event_id).to eq(completed_decision["award_event_id"])
+            expect(decision.rating_profile_date).to eq(completed_decision["rating_profile_date"])
+            expect(decision.decision_recorded_time).to eq(completed_decision["decision_recorded_time"])
+            expect(decision.decision_finalized_time).to eq(completed_decision["decision_finalized_time"])
+          when 987_876_765
+            expect(decision).not_to be_an_instance_of(CompletedDecision)
+            expect(decision).to be_nil
+          end
+        end
+      end
+
+      it "does not raise ArgumentError" do
+        expect { subject }.not_to raise_error
+      end
+    end
+
+    context "when Transformers::DecisionReviewCompleted portion of message_payload is invalid" do
+      context "because payload is nil" do
+        let(:nil_message_payload) { described_class.new(nil, nil) }
+
+        it "raises ArgumentError with message" do
+          error_message = "#{drc_class}: Message payload cannot be empty or nil"
+          expect { nil_message_payload }.to raise_error(ArgumentError, error_message)
+        end
+      end
+
+      context "because payload is empty" do
+        let(:empty_message_payload) { described_class.new(nil, {}) }
+
+        it "raises ArgumentError with message" do
+          error_message = "#{drc_class}: Message payload cannot be empty or nil"
+          expect { empty_message_payload }.to raise_error(ArgumentError, error_message)
+        end
+      end
+
+      context "because required attribute is missing" do
+        let(:message_payload_with_missing_attribute) do
+          message_payload.except("claim_id")
+        end
+
+        it "raises ArgumentError with message" do
+          error_message = "#{drc_class}: claim_id must be one of the allowed types - [Integer], got NilClass"
+          expect { described_class.new(event_id, message_payload_with_missing_attribute) }
+            .to raise_error(ArgumentError, error_message)
+        end
+      end
+
+      context "because payload has invalid attribute data type(s)" do
+        let(:message_payload_with_invalid_data_type) do
+          message_payload.merge(
+            "claim_id" => "87213"
+          )
+        end
+
+        it "raises ArgumentError with message" do
+          error_message = "#{drc_class}: claim_id must be one of the allowed types - [Integer], got String"
+          expect { described_class.new(event_id, message_payload_with_invalid_data_type) }
+            .to raise_error(ArgumentError, error_message)
+        end
+      end
+
+      context "because payload has unexpected attribute name(s)" do
+        let(:message_payload_with_multiple_invalid_attribute_names) do
+          message_payload.merge(
+            "invalid_attr" => "key",
+            "second_invalid_attr" => 123
+          )
+        end
+        it "logs all unknown attribute names" do
+          message = "#{drc_class}: Unknown attributes - invalid_attr, second_invalid_attr"
+          expect(Rails.logger).to receive(:info).with(message)
+          described_class.new(event_id, message_payload_with_multiple_invalid_attribute_names)
+        end
+      end
+    end
+
+    context "when DecisionReviewIssueCompleted portion of message_payload is invalid" do
+      context "because decision_review_issues_completed is an empty array" do
+        let(:message_payload_with_empty_decision_review_issues_completed) do
+          message_payload.merge(
+            "decision_review_issues_completed" => []
+          )
+        end
+
+        it "raises ArgumentError with message" do
+          error_message = "#{drc_class}: Message payload must include at least one decision review issue completed"
+          expect { described_class.new(event_id, message_payload_with_empty_decision_review_issues_completed) }
+            .to raise_error(ArgumentError, error_message)
+        end
+      end
+
+      context "because decision_review_issues_completed hash is empty" do
+        let(:message_payload_with_empty_issue) do
+          message_payload.merge(
+            "decision_review_issues_completed" => [{}]
+          )
+        end
+
+        it "raises ArgumentError with message" do
+          error_message = "#{decision_review_issue_completed_class}: Message payload cannot be empty or nil"
+          expect { described_class.new(event_id, message_payload_with_empty_issue) }
+            .to raise_error(ArgumentError, error_message)
+        end
+      end
+
+      context "because required attribute is missing" do
+        let(:decision_review_issue_completed_with_missing_attr) do
+          completed_decision_review_issue.except("decision_review_issue_id")
+        end
+        let(:message_payload_with_missing_attribute) do
+          message_payload.merge(
+            "decision_review_issues_completed" => [decision_review_issue_completed_with_missing_attr]
+          )
+        end
+
+        it "raises ArgumentError with message" do
+          error_message = "#{decision_review_issue_completed_class}: decision_review_issue_id must be"\
+          " one of the allowed types - [Integer], got NilClass"
+          expect { described_class.new(event_id, message_payload_with_missing_attribute) }
+            .to raise_error(ArgumentError, error_message)
+        end
+      end
+
+      context "because decision_review_issues_completed has invalid data type(s)" do
+        let(:decision_review_issue_completed_with_invalid_data_type) do
+          completed_decision_review_issue.merge(
+            "contention_id" => "string data type"
+          )
+        end
+        let(:message_payload_with_invalid_issue_data_type) do
+          message_payload.merge(
+            "decision_review_issues_completed" => [decision_review_issue_completed_with_invalid_data_type]
+          )
+        end
+
+        it "raises ArgumentError with message" do
+          error_message = "#{decision_review_issue_completed_class}: contention_id must be one of the allowed types"\
+          " - [Integer, NilClass], got String"
+          expect { described_class.new(event_id, message_payload_with_invalid_issue_data_type) }
+            .to raise_error(ArgumentError, error_message)
+        end
+      end
+
+      context "because decision_review_issues_completed has unexpected attribute name(s)" do
+        let(:decision_review_issue_completed_with_invalid_attr_names) do
+          completed_decision_review_issue.merge(
+            "invalid_attr" => "string",
+            "second_invalid_attr" => 1234
+          )
+        end
+        let(:message_payload_with_invalid_issue_attr_names) do
+          message_payload.merge(
+            "decision_review_issues_completed" => [decision_review_issue_completed_with_invalid_attr_names]
+          )
+        end
+
+        it "logs all unknown attribute names" do
+          message = "#{decision_review_issue_completed_class}: Unknown attributes - invalid_attr, "\
+          "second_invalid_attr"
+          expect(Rails.logger).to receive(:info).with(message)
+          described_class.new(event_id, message_payload_with_invalid_issue_attr_names)
+        end
+      end
+    end
+
+    context "when CompletedDecision portion of message_payload is invalid" do
+      context "because decision is an empty hash" do
+        let(:decision_review_issue_completed_with_nil_decision) do
+          completed_decision_review_issue.merge(
+            "decision" => {}
+          )
+        end
+        let(:message_payload_with_nil_decision) do
+          message_payload.merge(
+            "decision_review_issues_completed" => [decision_review_issue_completed_with_nil_decision]
+          )
+        end
+
+        it "raises ArgumentError with message" do
+          error_message = "#{decision_class}: Message payload cannot be empty"
+          expect { described_class.new(event_id, message_payload_with_nil_decision) }
+            .to raise_error(ArgumentError, error_message)
+        end
+      end
+
+      context "because required attribute is missing" do
+        let(:decision_review_issue_completed_with_missing_attr) do
+          completed_decision_review_issue.merge(
+            "decision" => decision_with_missing_attribute
+          )
+        end
+        let(:message_payload_with_missing_attribute) do
+          message_payload.merge(
+            "decision_review_issues_completed" => [decision_review_issue_completed_with_missing_attr]
+          )
+        end
+
+        it "raises ArgumentError with message" do
+          error_message = "#{decision_class}: disposition must be one of the allowed types - [String], got NilClass"
+          expect { described_class.new(event_id, message_payload_with_missing_attribute) }
+            .to raise_error(ArgumentError, error_message)
+        end
+      end
+
+      context "because decision has invalid data type(s)" do
+        let(:decision_review_issue_completed_with_invalid_attr_types) do
+          completed_decision_review_issue.merge(
+            "decision" => decision_with_invalid_attr_types
+          )
+        end
+        let(:message_payload_with_invalid_attr_types) do
+          message_payload.merge(
+            "decision_review_issues_completed" => [decision_review_issue_completed_with_invalid_attr_types]
+          )
+        end
+
+        it "raises ArgumentError with message" do
+          error_message = "#{decision_class}: contention_id must be one of the allowed types - [Integer], got String"
+          expect { described_class.new(event_id, message_payload_with_invalid_attr_types) }
+            .to raise_error(ArgumentError, error_message)
+        end
+      end
+
+      context "because decision has unexpected attribute name(s)" do
+        let(:decision_review_issue_completed_with_invalid_attr_names) do
+          completed_decision_review_issue.merge(
+            "decision" => decision_with_invalid_attr_names
+          )
+        end
+        let(:message_payload_with_invalid_attr_names) do
+          message_payload.merge(
+            "decision_review_issues_completed" => [decision_review_issue_completed_with_invalid_attr_names]
+          )
+        end
+
+        it "logs all unknown attribute names" do
+          message = "#{decision_class}: Unknown attributes - invalid_attr, second_invalid_attr"
+          expect(Rails.logger).to receive(:info).with(message)
+          described_class.new(event_id, message_payload_with_invalid_attr_names)
+        end
+      end
+    end
+  end
+end
