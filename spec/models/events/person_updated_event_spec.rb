51c741e9e3d83879e7e4d2051be973045fa7c251
Noelle Adkin <98478937+NoelleAd@users.noreply.github.com>
2024-11-06 10:11:54 -0500

    Add testing to pending person updates

--- spec/models/events/person_updated_event_spec.rb
+++ spec/models/events/person_updated_event_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 describe Events::PersonUpdatedEvent, type: :model do
-  let(:event) { create(:person_updated_event) }
+  let(:event) { build(:person_updated_event) }
 
   describe "#process!" do
     let(:dto_builder_instance) { instance_double("Builders::PersonUpdated::DtoBuilder") }
@@ -71,6 +71,132 @@ describe Events::PersonUpdatedEvent, type: :model do
           .to have_received(:establish_person_updated_event_error!)
           .with(event.id, JSON.parse(event.message_payload)["participant_id"], error_message)
         expect(Rails.logger).to have_received(:error).with(/#{standard_error}/)
+      end
+    end
+    context "when person_updated_event_pending? is true" do
+      let(:event) do
+        build(
+          :person_updated_event,
+          message_payload: { participant_id: participant_id, update_time: update_time },
+          participant_id: participant_id
+        )
+      end
+      let(:created_event) do
+        create(
+          :person_updated_event,
+          message_payload: { participant_id: participant_id },
+          participant_id: participant_id,
+          state: "NOT_STARTED",
+          completed_at: nil
+        )
+      end
+      let(:participant_id) { "1129318238" }
+      let(:update_time) { "2023-07-01T00:00:00Z" }
+      let(:error_message) { "Event IDs still needing processing: #{created_event.id}" }
+
+      before do
+        created_event
+        allow(EventAudit).to receive(:create!).and_call_original
+      end
+
+      it "raises an error" do
+        expect { event.process! }.to raise_error(AppealsConsumer::Error::PriorPersonUpdatedEventsStillPendingError)
+      end
+    end
+  end
+
+  describe "#person_updated_events_pending?" do
+    let(:event) do
+      build(
+        :person_updated_event,
+        message_payload: { participant_id: participant_id, update_time: update_time },
+        participant_id: participant_id
+      )
+    end
+    let(:participant_id) { "1129318238" }
+    let(:update_time) { "2023-07-01T00:00:00Z" }
+    let(:current_event) { double("Event", update_time: update_time) }
+
+    context "when no events match the criteria" do
+      it "returns false" do
+        expect(event.send("person_updated_events_pending?")).to be_falsey
+      end
+    end
+
+    context "when there is a PersonUpdatedEvent with matching criteria" do
+      before do
+        create(
+          :person_updated_event,
+          message_payload: { participant_id: participant_id },
+          participant_id: participant_id,
+          state: "NOT_STARTED",
+          completed_at: nil
+        )
+      end
+
+      it "returns true" do
+        expect(event.send("person_updated_events_pending?")).to be_truthy
+      end
+    end
+
+    context "when there is a PersonUpdatedEvent with matching criteria" do
+      before do
+        create(
+          :decision_review_updated_event,
+          participant_id: participant_id,
+          message_payload: { "update_time" => "2022-06-01T00:00:00Z" },
+          state: "IN_PROGRESS",
+          completed_at: nil
+        )
+      end
+
+      it "returns true" do
+        expect(event.send("person_updated_events_pending?")).to be_truthy
+      end
+    end
+
+    context "when there are events with states other than the specified ones" do
+      before do
+        create(
+          :person_updated_event,
+          message_payload: { participant_id: participant_id },
+          participant_id: participant_id,
+          state: "PROCESSED",
+          completed_at: nil
+        )
+        create(
+          :person_updated_event,
+          participant_id: participant_id,
+          message_payload: { "update_time" => "2022-06-01T00:00:00Z" },
+          state: "PROCESSED",
+          completed_at: nil
+        )
+      end
+
+      it "returns false" do
+        expect(event.send("person_updated_events_pending?")).to be_falsey
+      end
+    end
+    context "when there are events with non-nil completed_at timestamps" do
+      before do
+        create(
+          :decision_review_created_event,
+          message_payload: { participant_id: participant_id },
+          participant_id: participant_id,
+          state: "NOT_STARTED",
+          completed_at: Time.current
+        )
+        create(
+          :person_updated_event,
+          participant_id: participant_id,
+          message_payload: { "update_time" => "2022-06-01T00:00:00Z" },
+          state: "PROCESSED",
+          completed_at: Time.current
+        )
+      end
+
+      it "returns false" do
+        expect(event.send("person_updated_events_pending?")).to be_falsey
       end
     end
   end
